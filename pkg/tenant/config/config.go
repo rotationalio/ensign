75c28c53715929e9a88089e9537a816b47147d5e
daniellemaxwell <danielle@rotational.io>
2022-09-28 09:17:03 -0400

    Config file

--- pkg/tenant/config/config.go
+++ pkg/tenant/config/config.go
@@ -17,24 +17,24 @@ import (
 // will need to be defined as properties of this Config.
 type Config struct {
 	Maintenance  bool                `default:"false"`                                    // $TENANT_MAINTENANCE
-	BindAddr     string              `split_words:"true" default:":8080"`                 // $TENANT_BIND_ADDR
+	BindAddr     string              `split_words:"true" default:":8088"`                 // $TENANT_BIND_ADDR
 	Mode         string              `default:"release"`                                  // $TENANT_MODE
 	LogLevel     logger.LevelDecoder `split_words:"true" default:"info"`                  // $TENANT_LOG_LEVEL
 	ConsoleLog   bool                `split_words:"true" default:"false"`                 // $TENANT_CONSOLE_LOG
-	AllowOrigins []string            `split_words:"true" default:"http://localhostL3000"` // $TENANT_ALLOW_ORIGINS
+	AllowOrigins []string            `split_words:"true" default:"http://localhost:3000"` // $TENANT_ALLOW_ORIGINS
 	Sentry       sentry.Config
-	processed    bool //is set when the config is properly processed from the environment
+	processed    bool // set when the config is properly procesesed from the environment
 }
 
-// New loads and parses the config from the environment and validates it. It is then
-// marked as processed so that external users can determine if the config is ready for use.
-// This should be the only way Config objects are created for use in the application.
+// New loads and parses the config from the environment and validates it, marking it as
+// processed so that external users can determine if the config is ready for use. This
+// should be the only way Config objects are created for use in the application.
 func New() (conf Config, err error) {
 	if err = envconfig.Process("tenant", &conf); err != nil {
 		return Config{}, err
 	}
 
-	// Ensures the Sentry release is named correctly
+	// Ensure the Sentry release is named correctly
 	if conf.Sentry.Release == "" {
 		conf.Sentry.Release = fmt.Sprintf("tenant@%s", pkg.Version())
 	}
@@ -47,12 +47,12 @@ func New() (conf Config, err error) {
 	return conf, nil
 }
 
-// Returns true if the config has not been processed correctly from the environment.
+// Returns true if the config has not been correctly processed from the environment.
 func (c Config) IsZero() bool {
 	return !c.processed
 }
 
-// Marks a manually constructed config as processed if it is valid.
+// Mark a manually constructed config as processed as long as it is valid.
 func (c Config) Mark() (_ Config, err error) {
 	if err = c.Validate(); err != nil {
 		return c, err
@@ -61,9 +61,9 @@ func (c Config) Mark() (_ Config, err error) {
 	return c, nil
 }
 
-// Custom validations are added here, especially validations that require one or more
-// fields to be processed before validation occurs. All nested config validation methods
-// are called here.
+// Custom validations are added here, particularly validations that require one or more
+// fields to be processed before the validation occurs.
+// NOTE: ensure that all nested config validation methods are called here.
 func (c Config) Validate() (err error) {
 	if c.Mode != gin.ReleaseMode && c.Mode != gin.DebugMode && c.Mode != gin.TestMode {
 		return fmt.Errorf("invalid configuration: %q is not a valid gin mode", c.Mode)
@@ -80,6 +80,7 @@ func (c Config) GetLogLevel() zerolog.Level {
 	return zerolog.Level(c.LogLevel)
 }
 
+// Returns true if the allow origins slice contains one entry that is a "*"
 func (c Config) AllowAllOrigins() bool {
 	if len(c.AllowOrigins) == 1 && c.AllowOrigins[0] == "*" {
 		return true
