1177345707423fb8a361315741dac9b02b126c9f
daniellemaxwell <danielle@rotational.io>
2022-09-30 11:14:47 -0400

    Added form with validation

---
+++ web/ensign-landing-page/src/Components/content/Access.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+export default function Access() {
+    return (
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          title: '',
+          organization: '',
+        }}
+        validationSchema={Yup.object().shape({
+          firstName: Yup.string().required('First Name is required'),
+          lastName: Yup.string().required('Last Name is required'),
+          email: Yup.string()
+            .email('Email is invalid')
+            .required('Email is required'),
+          title: Yup.string(),
+          organization: Yup.string(),
+        })}
+        onSubmit={(fields) => {
+          alert('SUCCESS!! :-)\n\n' + JSON.stringify(fields, null, 4));
+        }}
+        render={({ errors, status, touched }) => (
+          <Form>
+            <div>
+              <h3 class="text-2xl font-bold">Request Alpha Access Today</h3>
+              <p>We're opening up Ensign on a limited basis. No credit card required.</p>
+            </div>
+            <div className="form-group" class="pb-2">
+              <label htmlFor="firstName">First Name</label>
+              <Field
+                name="firstName"
+                type="text"
+                placeholder="*First Name"
+                className={
+                  'form-control' +
+                  (errors.firstName && touched.firstName ? ' is-invalid' : '')
+                }
+              />
+              <ErrorMessage
+                name="firstName"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="lastName">Last Name</label>
+              <Field
+                name="lastName"
+                type="text"
+                placeholder="*Last Name"
+                className={
+                  'form-control' +
+                  (errors.lastName && touched.lastName ? ' is-invalid' : '')
+                }
+              />
+              <ErrorMessage
+                name="lastName"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field
+                name="email"
+                type="text"
+                placeholder="*Email address"
+                className={
+                  'form-control' +
+                  (errors.email && touched.email ? ' is-invalid' : '')
+                }
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="invalid-feedback"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <Field name="title" type="title" placeholder="Title" className={'form-control'} />
+            </div>
+            <div className="form-group">
+              <label htmlFor="organization">Organization</label>
+              <Field
+                name="organization"
+                type="organization"
+                placeholder="Organization"
+                className={'form-control'}
+              />
+            </div>
+            <div class="pb-5">
+            <label>
+                <Field type="checkbox" name="notifications" value="notifications" />
+                I agree to receive notifications about Ensign from Rotational Labs. Your contact information will not be shared with external parties. Unsubscribe any time. 
+              </label>
+            </div>
+            <div className="form-group w-52 mx-auto p-1 text-2xl text-white bg-[#37A36E]">
+              <button type="submit">
+                Request Access
+              </button>
+            </div>
+          </Form>
+        )}
+      />
+    );
+  }
--- web/ensign-landing-page/src/Components/layout/Main.js
+++ web/ensign-landing-page/src/Components/layout/Main.js
@@ -1,29 +1,29 @@
 import React from "react";
+import Access from "../content/Access";
 
 export default function Main() {
     return(
         <main class="pb-20">
-            <section>
-                </section>
-                <section class="flex">
-                    <section class="shrink text-left w-1/2">
-                        <h2>Data Engineering Simplified.</h2>
-                        <h2>Navigate Your Data to Where It's Valued.</h2>
-                        <p>What value could you deliver if you could combine different data sources and types, and deliver in real-time with no additional infrastructure or admin burden?</p>
-                        <p>We've thought deeply about that question and the result is Ensign, our intelligent event-data platform for real-time apps and analytics. Designed to be accessible to everyday builders and organizations. Ensign makes it easy to:</p>
-                        <ul>
-                            <li>Customize your data pipelines</li>
-                            <li>Quickly build or integrate events into new or existing applications</li>
-                            <li>Provide fast, consistent, and personalized digital experiences across time and space</li>
-                            <li>Accelerate time-to-insight in business intelligence and data analytics</li>
-                        </ul>
-                        <p>Even better, Ensign grows with you with built-in geo-scaling, data compliance, and diasaster recovery controls.</p>
-                    </section>
-                    <section>
-    
-                    </section>
+            <section class="pb-10">
+                <h2 class="leading-10">Data Engineering Simplified.</h2>
+                <h2>Navigate Your Data to Where It's Valued.</h2>
             </section>
-            
+            <section class="flex">
+                <section class="text-left w-1/2">
+                    <p class="pb-5">What value could you deliver if you could combine different data sources and types, and deliver in real-time with no additional infrastructure or admin burden?</p>
+                    <p class="pb-5">We've thought deeply about that question and the result is <span class="text-[#1D65A6] font-bold">Ensign</span>, our intelligent event-data platform for real-time apps and analytics. Designed to be accessible to everyday builders and organizations. Ensign makes it easy to:</p>
+                    <ul class="list-disc list-inside pb-5">
+                        <li>Customize your data pipelines</li>
+                        <li>Quickly build or integrate events into new or existing applications</li>
+                        <li>Provide fast, consistent, and personalized digital experiences across time and space</li>
+                        <li>Accelerate time-to-insight in business intelligence and data analytics</li>
+                    </ul>
+                    <p>Even better, Ensign grows with you with built-in geo-scaling, data compliance, and diasaster recovery controls.</p>
+                </section>
+                <section class="w-1/2 bg-[#DED6C5]">
+                    <Access />
+                </section>
+            </section>
         </main>
     )
 }
