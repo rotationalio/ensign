af2c4e673ca9f61ac28bc181d6f584f111cee735
daniellemaxwell <danielle@rotational.io>
2022-10-20 11:16:04 -0400

    Analytics console update

--- web/ensign-landing-page/src/lib/analytics.js
+++ web/ensign-landing-page/src/lib/analytics.js
@@ -16,7 +16,7 @@ const withTracker = (Component) => (props) => {
       case false:
         const ga4react = new GA4React(gaCode);
         ga4react.initialize().then((ga4) => {
-          console.log(`tracking initialized with ${gaCode}`);
+          console.log("Google Analytics tracking initialized");
           ga4.pageview(window.location.pathname);
         }, (err) => {
           console.error(err);
