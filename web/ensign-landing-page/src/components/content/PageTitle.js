7cfab71423b2c2fcad443842b36c05a6c335effe
daniellemaxwell <danielle@rotational.io>
2022-10-24 14:34:36 -0400

    Header text corrections

--- web/ensign-landing-page/src/components/content/PageTitle.js
+++ web/ensign-landing-page/src/components/content/PageTitle.js
@@ -4,9 +4,9 @@ export default function PageTitle() {
     return (
         <section className="text-white text-left">
             <div className="container pl-3 pt-5 sm:pl-20">
-              <h1 className="pb-1 text-xl sm:text-3xl md:text-6xl xl:text-7xl">Ensign</h1>
-              <h2 className="font-light text-base sm:text-base md:text-2xl lg:text-4xl">Powering Realtime Experiences</h2>
-              <h2 className="font-light text-base sm:text-base md:text-2xl lg:text-4xl">&amp; Data-driven Apps</h2>
+              <h1 className="pb-1 text-3xl sm:text-3xl lg:text-5xl xl:text-7xl">Ensign</h1>
+              <h2 className="text-base md:text-2xl lg:text-3xl">Powering Realtime Experiences</h2>
+              <h2 className="text-base md:text-2xl lg:text-3xl lg:text-5xl">&amp; Data-driven Apps</h2>
             </div>
         </section>
     )
