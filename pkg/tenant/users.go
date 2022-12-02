79f826716a97e3a15a1f9dd7d08ccf0968dd775e
daniellemaxwell <danielle@rotational.io>
2022-12-02 15:12:54 -0500

    TenantList and TenantDelete handlers

--- pkg/tenant/users.go
+++ pkg/tenant/users.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/rotationalio/ensign/pkg/tenant/api/v1"
+	"github.com/rotationalio/ensign/pkg/tenant/db"
+	"github.com/rs/zerolog/log"
 )
 
 func TenantList(c *gin.Context) {
@@ -14,14 +18,61 @@ func TenantCreate(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
-func TenantDetail(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "not implemented yet")
+func (s *Server) TenantDetail(c *gin.Context) {
+	var (
+		err    error
+		tenant *api.Tenant
+	)
+
+	if tenant.ID == "" {
+		log.Error().Err(err).Msg(db.ErrMissingID.Error())
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not retrieve tenant"))
+		return
+	}
+
+	// TODO: Replace uuid with ulid
+	req := &db.Tenant{
+		ID: uuid.UUID{},
+	}
+
+	if _, err = db.RetrieveTenant(c.Request.Context(), req.ID); err != nil {
+		log.Error().Err(err).Msg(db.ErrMissingID.Error())
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not retrieve tenant"))
+		return
+	}
+
+	c.JSON(http.StatusOK, tenant)
 }
 
 func TenantUpdate(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
-func TenantDelete(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "not implemented yet")
+func (s *Server) TenantDelete(c *gin.Context) {
+	var (
+		err error
+		out *api.Reply
+	)
+
+	tenantID := c.Param("tenantID")
+
+	if tenantID == "" {
+		log.Error().Err(err).Msg(db.ErrMissingID.Error())
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not retrieve tenant"))
+		return
+	}
+
+	// TODO: Replace uuid with ulid
+	req := &db.Tenant{
+		ID: uuid.UUID{},
+	}
+
+	// Delete Tenant and
+	if err = db.DeleteTenant(c.Request.Context(), req.ID); err != nil {
+		log.Error().Err(err).Msg(db.ErrMissingID.Error())
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not retrieve tenant"))
+		return
+	}
+
+	c.JSON(http.StatusOK, out)
 }
