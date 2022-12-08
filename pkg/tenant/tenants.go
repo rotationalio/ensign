0aefbc6b09b4bddb2d203516fd1ee323ddca4827
daniellemaxwell <danielle@rotational.io>
2022-12-08 11:24:38 -0500

    TenantUpdate server handler

--- pkg/tenant/tenants.go
+++ pkg/tenant/tenants.go
@@ -11,58 +11,26 @@ import (
 )
 
 func (s *Server) TenantList(c *gin.Context) {
-
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
 func (s *Server) TenantCreate(c *gin.Context) {
-	var (
-		err    error
-		tenant *api.Tenant
-	)
-
-	// TODO: Add authentication and authorization middleware
-
-	if err = c.BindJSON(&tenant); err != nil {
-		log.Warn().Err(err).Msg("could not parse tenant create request")
-		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not bind request"))
-	}
-
-	if tenant.ID == "" {
-		log.Error().Err(err).Msg("tenant does not have an ID")
-	}
-
-	if tenant.Name == "" {
-		log.Warn().Err(err).Msg("tenant does not have an name")
-	}
-
-	if tenant.EnvironmentType == "" {
-		log.Error().Err(err).Msg("tenant does not have an environment type")
-	}
-
-	if err = db.CreateTenant(c.Request.Context(), &db.Tenant{ID: ulid.Make()}); err != nil {
-		log.Error().Err(err).Msg("could not save tenant")
-		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not add tenant"))
-	}
-
-	c.JSON(http.StatusCreated, tenant)
+	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
 func (s *Server) TenantDetail(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
+// TenantUpdate will amend the name or environment type of a tenant
 func (s *Server) TenantUpdate(c *gin.Context) {
 	var (
 		err error
-		in  *api.Tenant
+		in  *db.Tenant
 		out *api.Tenant
 	)
 
 	// TODO: authentication and authorization middleware
-
-	// Get ID from param
-	tenantID := c.Param("tenantID")
 
 	// Bind JSON insertion
 	if err = c.BindJSON(&in); err != nil {
@@ -70,22 +38,24 @@ func (s *Server) TenantUpdate(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not bind request"))
 	}
 
-	// Confirm ID exists
-	if tenantID == "" {
-		log.Error().Err(err).Msg("tenant does not have an ID")
+	var tenantID ulid.ULID
+	if tenantID, err = ulid.Parse(c.Param("tenantID")); err != nil {
+		log.Debug().Err(err).Msg("could not parse tenant ulid")
+		c.JSON(http.StatusNotFound, api.ErrorResponse("tenant not found"))
+		return
 	}
 
 	// Update tenant with a new ID and add to the database
-	if err = db.UpdateTenant(c.Request.Context(), &db.Tenant{}); err != nil {
+	if err = db.UpdateTenant(c.Request.Context(), &db.Tenant{ID: tenantID}); err != nil {
 		log.Error().Err(err).Msg("could not save tenant")
 		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not update tenant"))
 	}
 
 	// Prepare what will go out
 	out = &api.Tenant{
-		ID:              in.ID,
-		Name:            in.Name,
-		EnvironmentType: in.EnvironmentType,
+		ID:              out.ID,
+		Name:            out.Name,
+		EnvironmentType: out.EnvironmentType,
 	}
 
 	c.JSON(http.StatusOK, out)
