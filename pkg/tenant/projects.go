5c5f2e263eaab9d667bf23671aa9ad17645192a5
daniellemaxwell <danielle@rotational.io>
2022-12-28 11:21:40 -0500

    Add ProjectDetail, ProjectUpdate, and ProjectDelete

--- pkg/tenant/projects.go
+++ pkg/tenant/projects.go
@@ -4,6 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/oklog/ulid/v2"
+	"github.com/rotationalio/ensign/pkg/tenant/api/v1"
+	"github.com/rotationalio/ensign/pkg/tenant/db"
+	"github.com/rs/zerolog/log"
 )
 
 func (s *Server) TenantProjectList(c *gin.Context) {
@@ -31,18 +35,121 @@ func (s *Server) ProjectList(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
-func (s *Server) ProjectDetail(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "not implemented yet")
-}
-
 func (s *Server) ProjectCreate(c *gin.Context) {
 	c.JSON(http.StatusNotImplemented, "not implemented yet")
 }
 
-func (s *Server) ProjectUpdate(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "not implemented yet")
+// ProjectDetail retrieves a summary detail of a project by its
+// ID and returns a 200 OK response.
+//
+// Route: /project/:projectID
+func (s *Server) ProjectDetail(c *gin.Context) {
+	var (
+		err   error
+		reply *api.Project
+	)
+
+	// Get the project ID from the URL and return a 400 response
+	// if the project does not exist.
+	var projectID ulid.ULID
+	if projectID, err = ulid.Parse(c.Param("projectID")); err != nil {
+		log.Error().Err(err).Msg("could not parse project ulid")
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not parse project id"))
+		return
+	}
+
+	// Get the specified project from the database and return a 404 response
+	// if it cannot be retrieved.
+	var project *db.Project
+	if project, err = db.RetrieveProject(c.Request.Context(), projectID); err != nil {
+		log.Error().Err(err).Str("projectID", projectID.String()).Msg("could not retrieve project")
+		c.JSON(http.StatusNotFound, api.ErrorResponse("could not retrieve project"))
+		return
+	}
+
+	reply = &api.Project{
+		ID:   project.ID.String(),
+		Name: project.Name,
+	}
+
+	c.JSON(http.StatusOK, reply)
 }
 
+// ProjectUpdate updates the record of a project with a given ID
+// and returns a 200 OK response.
+//
+// Route: /project/:projectID
+func (s *Server) ProjectUpdate(c *gin.Context) {
+	var (
+		err     error
+		project *api.Project
+	)
+
+	// Get the project ID from the URL and return a 400 response if
+	// the project ID is not a ULID.
+	var projectID ulid.ULID
+	if projectID, err = ulid.Parse(c.Param("projectID")); err != nil {
+		log.Error().Err(err).Msg("could not parse project id")
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not parse project id"))
+		return
+	}
+
+	// Bind the user request with JSON and return a 400 response
+	// if binding is not successful.
+	if err = c.BindJSON(&project); err != nil {
+		log.Warn().Err(err).Msg("could not parse project update request")
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not bind user request"))
+		return
+	}
+
+	// Verify that the project name exists and return a 400 response if it doesn't.
+	if project.Name == "" {
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("project name is required"))
+		return
+	}
+
+	// Get the specified project from the database and return a 404 response if
+	// it cannot be retrieved.
+	var p *db.Project
+	if p, err = db.RetrieveProject(c.Request.Context(), projectID); err != nil {
+		log.Error().Err(err).Str("projectID", projectID.String()).Msg("could not retrieve project")
+		c.JSON(http.StatusNotFound, api.ErrorResponse("project not found"))
+		return
+	}
+
+	// Update project in the database and return a 500 response if the
+	// project record cannot be updated.
+	if err = db.UpdateProject(c.Request.Context(), p); err != nil {
+		log.Error().Err(err).Str("projectID", projectID.String()).Msg("could not save project")
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not update project"))
+		return
+	}
+	c.JSON(http.StatusOK, project)
+}
+
+// ProjectDelete deletes a project from a user's request with a given ID
+// and returns a 200 OK response instead of an error response.
+//
+// Route: /project/:projectID
 func (s *Server) ProjectDelete(c *gin.Context) {
-	c.JSON(http.StatusNotImplemented, "not implemented yet")
+	var (
+		err error
+	)
+
+	// Get the project ID from the URL and return a 400 response
+	// if the project does not exist.
+	var projectID ulid.ULID
+	if projectID, err = ulid.Parse(c.Param("projectID")); err != nil {
+		log.Error().Err(err).Msg("could not parse project ulid")
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not parse project id"))
+		return
+	}
+
+	// Delete the project and return a 404 response if it cannot be removed.
+	if err = db.DeleteProject(c.Request.Context(), projectID); err != nil {
+		log.Error().Err(err).Str("projectID", projectID.String()).Msg("could not delete project")
+		c.JSON(http.StatusNotFound, api.ErrorResponse("could not delete project"))
+		return
+	}
+	c.Status(http.StatusOK)
 }
