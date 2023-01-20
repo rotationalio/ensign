f90b5b72dd4c5ec6b0b8d95f634cb9ccd9d7d242
Benjamin Bengfort <benjamin@bengfort.com>
2023-01-20 11:14:53 -0600

    address review comments

--- pkg/quarterdeck/keygen/keygen.go
+++ pkg/quarterdeck/keygen/keygen.go
@@ -18,28 +18,27 @@ const (
 	idxmax   = 63 / idxbits
 )
 
+// Alpha generates a random string of n characters that only includes upper and
+// lowercase letters (no symbols or digits).
 func Alpha(n int) string {
-	sb := strings.Builder{}
-	sb.Grow(n)
-
-	for i, cache, remain := n-1, CryptoRandInt(), idxmax; i >= 0; {
-		if remain == 0 {
-			cache, remain = CryptoRandInt(), idxmax
-		}
-
-		if idx := int(cache & idxmask); idx < len(alphabet) {
-			sb.WriteByte(alphabet[idx])
-			i--
-		}
-
-		cache >>= idxbits
-		remain--
-	}
-
-	return sb.String()
+	return generate(n, alphabet)
 }
 
+// AlphaNumeric generates a random string of n characters that includes upper and
+// lowercase letters and the digits 0-9.
 func AlphaNumeric(n int) string {
+	return generate(n, alphanum)
+}
+
+// generate is a helper function to create a random string of n characters from the
+// character set defined by chars. It uses as efficient a method of generation as
+// possible, using a string builder to prevent multiple allocations and a 6 bit mask
+// to select 10 random letters at a time to add to the string. This method would be far
+// faster if it used math/rand src and the Int63() function, but for API key generation
+// it is important to use a cryptographically random generator.
+//
+// See: https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+func generate(n int, chars string) string {
 	sb := strings.Builder{}
 	sb.Grow(n)
 
@@ -48,8 +47,8 @@ func AlphaNumeric(n int) string {
 			cache, remain = CryptoRandInt(), idxmax
 		}
 
-		if idx := int(cache & idxmask); idx < len(alphanum) {
-			sb.WriteByte(alphanum[idx])
+		if idx := int(cache & idxmask); idx < len(chars) {
+			sb.WriteByte(chars[idx])
 			i--
 		}
 
