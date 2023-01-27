1075d1d62a8ea15a57c390fefd4519f52dcf89dd
daniellemaxwell <danielle@rotational.io>
2023-01-26 20:19:28 -0500

    Add color to span

--- web/beacon-app/src/components/MaintenanceMode/MaintenaceMode.tsx
+++ web/beacon-app/src/components/MaintenanceMode/MaintenaceMode.tsx
@@ -2,11 +2,14 @@ import busyOtters from '/src/assets/images/busy-sea-otters.png';
 
 export default function MaintenaceMode() {
   return (
-    <section className="max-w-4xl rounded-lg border border-solid border-primary-800 text-2xl">
+    <section className="mx-auto max-w-4xl rounded-lg border border-solid border-primary-800 text-2xl">
       <p className="mx-auto mt-8 max-w-[600px]">
         Ensign is temporarily undergoing scheduled maintenace. We&#39;ll be back online shortly. See
-        our <a href="#">operating status</a> for additional information. Contact us any time with
-        questions.
+        our{' '}
+        <span className="font-bold text-primary">
+          <a href="#">operating status</a>
+        </span>{' '}
+        for additional information. Contact us any time with questions.
       </p>
       <img src={busyOtters} alt="" className="mx-auto mt-10" />
       <p className="mt-8 pb-20 text-center">
