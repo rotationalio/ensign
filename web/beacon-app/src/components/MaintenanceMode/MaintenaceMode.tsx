c01d024c5ce87e66269165a0f7a00bdc5f52e920
daniellemaxwell <danielle@rotational.io>
2023-01-26 20:10:41 -0500

    Add style to component

--- web/beacon-app/src/components/MaintenanceMode/MaintenaceMode.tsx
+++ web/beacon-app/src/components/MaintenanceMode/MaintenaceMode.tsx
@@ -1,20 +1,20 @@
-import busyOtters from '/src/components/MaintenanceMode/busy_sea_otters.png';
+import busyOtters from '/src/assets/images/busy-sea-otters.png';
 
-function MaintenaceMode() {
+export default function MaintenaceMode() {
   return (
-    <section>
-      <p>Ensign is temporarily undergoing scheduled maintenace.</p>
-      <p>
-        We&#39;ll be back online shortly. See our <a href="#">operating status</a> for additional
-        information. Contact us any time with questions.
+    <section className="max-w-4xl rounded-lg border border-solid border-primary-800 text-2xl">
+      <p className="mx-auto mt-8 max-w-[600px]">
+        Ensign is temporarily undergoing scheduled maintenace. We&#39;ll be back online shortly. See
+        our <a href="#">operating status</a> for additional information. Contact us any time with
+        questions.
       </p>
-      <img src={busyOtters} alt="" />
-      <p>
+      <img src={busyOtters} alt="" className="mx-auto mt-10" />
+      <p className="mt-8 pb-20 text-center">
         Enjoy a cup of coffee and catch up on our latest{' '}
-        <a href="https://rotational.io/blog">blog posts!</a>
+        <span className="font-bold text-primary">
+          <a href="https://rotational.io/blog">blog posts!</a>
+        </span>
       </p>
     </section>
   );
 }
-
-export default MaintenaceMode;
--- web/beacon-app/src/main.tsx
+++ web/beacon-app/src/main.tsx
@@ -7,12 +7,10 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@/application/routes/root';
 
 import App from './App';
-import MaintenaceMode from './components/MaintenanceMode/MaintenaceMode';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <App />
-    <MaintenaceMode />
   </React.StrictMode>
 );
