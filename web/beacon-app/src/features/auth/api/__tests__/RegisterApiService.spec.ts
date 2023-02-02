20d3407de60584cadc2c7707ecd1d991fb069c02
Cletus Razakou <masskoder@gmail.com>
2023-02-02 14:40:01 -0500

    refact: add missing registration request params

--- web/beacon-app/src/features/auth/api/__tests__/RegisterApiService.spec.ts
+++ web/beacon-app/src/features/auth/api/__tests__/RegisterApiService.spec.ts
@@ -5,7 +5,17 @@ import { createAccountRequest } from '../RegisterApiService';
 describe('AccountApiService', () => {
   describe('createAccountRequest', () => {
     it('returns request resolved with response', async () => {
-      const mockAccount = { email: 'test@ensign.com', password: 'test', username: 'test' };
+      const mockAccount = {
+        name: 'name',
+        organization: 'organization',
+        domain: 'domain',
+        terms_agreement: true,
+        privacy_agreement: true,
+        email: 'test@rotational.io',
+        password: 'password',
+        pwcheck: 'password',
+
+      };
 
       const requestSpy = vi.fn().mockReturnValueOnce({
         status: 200,
