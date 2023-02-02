a844bb5a75378e9ed5b001b40be2992a03ec70f5
Danielle <danielle@rotational.io>
2023-02-02 18:44:51 -0500

    Cancel Account Modal (#142)

---
+++ web/beacon-app/src/components/ui/CancelModal/CancelAcctModal.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, Story } from '@storybook/react';
+import CancelAcctModal from './CancelAcctModal';
+
+export default {
+  title: 'beacon/CancelAcctModal',
+} as Meta;
+
+const Template: Story = (args) => <CancelAcctModal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
---
+++ web/beacon-app/src/components/ui/CancelModal/index.ts
@@ -0,0 +1,1 @@
+export { default as CancelAcctModal } from './CancelAcctModal'
