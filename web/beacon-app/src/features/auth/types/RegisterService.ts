41dec52585737d74828770827b58936542633bd2
Cletus Razakou <masskoder@gmail.com>
2023-02-02 16:13:14 -0500

    refact: add missing registration request params (#156)

--- web/beacon-app/src/features/auth/types/RegisterService.ts
+++ web/beacon-app/src/features/auth/types/RegisterService.ts
@@ -1,9 +1,30 @@
 export interface User {
-  id: string;
-  full_name?: string;
+  user_id: string;
+  name: string;
+  pwcheck: string;
+  organization: string;
+  domain: string;
+  terms_agreement: boolean;
+  privacy_agreement: boolean;
   email: string;
-  username: string;
   password: string;
+
 }
 
-export type NewUserAccount = Omit<User, 'id'>;
+export interface NewUserResponseData {
+  user_id: string;
+  org_id: string;
+  email: string;
+  message: string;
+  role: string;
+  created: string;
+}
+
+
+export type NewUserAccount = Omit<User, 'user_id'>;
+
+export const hasUserRequiredFields = (account: NewUserAccount): account is Required<NewUserAccount> => {
+  return Object.values(account).every(x => !!x) &&
+    account.terms_agreement === true && account.privacy_agreement === true;
+};
+
