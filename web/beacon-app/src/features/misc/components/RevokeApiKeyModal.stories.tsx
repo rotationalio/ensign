56773ead76dee014c9736293e79f3e30b07e3e08
elysee15 <elyseebleu1@gmail.com>
2023-02-06 20:13:19 +0000

    sc-13389 Beacon FE: Revoke API Modal Component

---
+++ web/beacon-app/src/features/misc/components/RevokeApiKeyModal.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, Story } from '@storybook/react';
+
+import RevokeApiKeyModal, { RevokeApiKeyModalProps } from './RevokeApiKeyModal';
+
+export default {
+  title: 'beacon/RevokeApiKeyModal',
+} as Meta<RevokeApiKeyModalProps>;
+
+const Template: Story<RevokeApiKeyModalProps> = (args) => <RevokeApiKeyModal {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  open: true,
+};
