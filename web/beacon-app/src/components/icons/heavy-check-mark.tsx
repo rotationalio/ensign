90b280e8316b0e9dd0026f89d14f7467bfeda70a
elysee15 <elyseebleu1@gmail.com>
2023-02-06 19:51:56 +0000

    sc-12463 Beacon FE: add successfully modal component after tenant creation

---
+++ web/beacon-app/src/components/icons/heavy-check-mark.tsx
@@ -0,0 +1,21 @@
+export default function HeavyCheckMark(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      width="123"
+      height="122"
+      viewBox="0 0 123 122"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M61.5 118.188C93.3427 118.188 119.156 92.5838 119.156 61C119.156 29.4162 93.3427 3.8125 61.5 3.8125C29.6573 3.8125 3.84375 29.4162 3.84375 61C3.84375 92.5838 29.6573 118.188 61.5 118.188Z"
+        fill="#37A36E"
+      />
+      <path
+        d="M88.4062 26.6875L48.0469 67.8625L34.5938 54.1375L21.1406 67.8625L48.0469 95.3125L101.859 40.4125L88.4062 26.6875Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
