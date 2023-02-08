05085add8c3aa787dc7007abe9cd2ca85fca2312
Cletus Razakou <masskoder@gmail.com>
2023-02-08 17:16:24 -0500

    fix: typo

--- web/beacon-app/src/components/common/QuickView/QuickView.tsx
+++ web/beacon-app/src/components/common/QuickView/QuickView.tsx
@@ -5,7 +5,7 @@ interface QuickViewData {
   value: number;
 }
 
-const QUICKVIEW_LIMIT = 4; // we should have 4 items in the quick view
+const QUICKVIEW_CARD_LENGTH = 4; // we should have 4 items in the quick view
 
 export interface QuickViewProps {
   data: QuickViewData[];
@@ -15,7 +15,7 @@ const BRAND_COLORS = ['#ECF6FF', '#FFE9DD', '#ECFADC', '#FBF8EC'];
 const QuickView: React.FC<QuickViewProps> = ({ data }) => {
   return (
     <div className="grid grid-cols-2 gap-y-10 gap-x-20 lg:grid-cols-4">
-      {data?.length === QUICKVIEW_LIMIT &&
+      {data?.length === QUICKVIEW_CARD_LENGTH &&
         data.map((item, index) => (
           <QuickViewCard key={item.name} title={item.name} color={BRAND_COLORS[index]}>
             {item.value}
