df066f093e65666c4a10de533113c066d6109148
Cletus Razakou <masskoder@gmail.com>
2023-02-08 12:16:09 -0500

    clean up

--- web/beacon-app/src/application/config/appConfig.tsx
+++ web/beacon-app/src/application/config/appConfig.tsx
@@ -1,6 +1,6 @@
 const appConfig = {
   quaterDeckApiUrl: import.meta.env.REACT_APP_QUARTERDECK_BASE_URL || 'http://localhost:8088/v1',
-  tenantApiUrl: import.meta.env.REACT_APP_TENANT_BASE_URL || 'http://localhost:3636/v1',
+  tenantApiUrl: import.meta.env.REACT_APP_TENANT_BASE_URL || 'http://localhost:8080/v1',
   sentryDSN: import.meta.env.REACT_APP_SENTRY_DSN,
   sentryENV: import.meta.env.REACT_APP_SENTRY_ENVIRONMENT,
   nodeENV: import.meta.env.NODE_ENV,
--- web/beacon-app/src/components/common/Logo.tsx
+++ web/beacon-app/src/components/common/Logo.tsx
@@ -6,7 +6,7 @@ function Logo() {
   return (
     <Link to="/" data-testid="logo">
       <div className="flex items-center space-x-2">
-        <img src={RotationalLogo} alt="Rotational Lab" className="h-12 w-12" />
+        <img src={RotationalLogo} alt="Rotational Labs" className="h-12 w-12" />
         <h1 className="text-2xl font-bold text-primary">Rotational Labs</h1>
       </div>
     </Link>
