2f2e4b24877467e59b683b15014b1219529adefa
Benjamin Bengfort <benjamin@bengfort.com>
2023-02-10 14:06:06 -0600

    gRPC Liveness Probe

---
+++ pkg/utils/probez/grpc/v1/probez_test.go
@@ -0,0 +1,234 @@
+package health_test
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/rotationalio/ensign/pkg/utils/bufconn"
+	. "github.com/rotationalio/ensign/pkg/utils/probez/grpc/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func TestProbezDefaultStatus(t *testing.T) {
+	srv := &ProbeServer{}
+
+	// Status before default service has been set
+	require.Equal(t, HealthCheckResponse_UNKNOWN, srv.Status("", false))
+	require.Equal(t, HealthCheckResponse_SERVICE_UNKNOWN, srv.Status("", true))
+	require.Equal(t, srv.Status(DefaultService, false), srv.Status("", false))
+	require.Equal(t, srv.Status(DefaultService, true), srv.Status("", true))
+
+	// Mark default service as healthy
+	srv.Healthy()
+	require.Equal(t, StatusServing, srv.Status("", false))
+	require.Equal(t, StatusServing, srv.Status("", true))
+	require.Equal(t, srv.Status(DefaultService, false), srv.Status("", false))
+	require.Equal(t, srv.Status(DefaultService, true), srv.Status("", true))
+
+	// Mark default service as not healthy
+	srv.NotHealthy()
+	require.Equal(t, StatusNotServing, srv.Status("", false))
+	require.Equal(t, StatusNotServing, srv.Status("", true))
+	require.Equal(t, srv.Status(DefaultService, false), srv.Status("", false))
+	require.Equal(t, srv.Status(DefaultService, true), srv.Status("", true))
+}
+
+func TestProbezServiceStatus(t *testing.T) {
+	srv := &ProbeServer{}
+	services := []string{
+		"", "foo", "bar", "health",
+	}
+
+	for i, service := range services {
+		// Status should be unknown before it has been set
+		require.Equal(t, HealthCheckResponse_UNKNOWN, srv.Status(service, false))
+		require.Equal(t, HealthCheckResponse_SERVICE_UNKNOWN, srv.Status(service, true))
+
+		srv.SetStatus(service, StatusNotServing)
+		require.Equal(t, StatusNotServing, srv.Status(service, false))
+		require.Equal(t, StatusNotServing, srv.Status(service, true))
+
+		// The other service statuses should not have changed
+		for j, otherService := range services {
+			if i == j {
+				continue
+			}
+
+			if j < i {
+				require.Equal(t, StatusServing, srv.Status(otherService, false))
+				require.Equal(t, StatusServing, srv.Status(otherService, true))
+			} else {
+				require.Equal(t, HealthCheckResponse_UNKNOWN, srv.Status(otherService, false))
+				require.Equal(t, HealthCheckResponse_SERVICE_UNKNOWN, srv.Status(otherService, true))
+			}
+		}
+
+		srv.SetStatus(service, StatusServing)
+		require.Equal(t, StatusServing, srv.Status(service, false))
+		require.Equal(t, StatusServing, srv.Status(service, true))
+
+		require.Equal(t, srv.Status(DefaultService, false), srv.Status("", false))
+		require.Equal(t, srv.Status(DefaultService, true), srv.Status("", true))
+	}
+}
+
+func TestWatchers(t *testing.T) {
+	var wg sync.WaitGroup
+	var watchersReady sync.WaitGroup
+	counts := make([]int, 5)
+	srv := &ProbeServer{}
+
+	// Spin 5 watchers, 3 for default, 2 for ensign
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		watchersReady.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			service := DefaultService
+			if idx > 2 {
+				service = "ensign.v1.Ensign"
+			}
+
+			id := fmt.Sprintf("watcher-%d", idx)
+			watcher := srv.AddWatcher(id, service)
+			watchersReady.Done()
+
+			for range watcher {
+				counts[idx]++
+			}
+		}(i)
+	}
+
+	// Wait until all the watchers are ready before changing statuses.
+	watchersReady.Wait()
+
+	// Spin several go routines that change the status of the service and delete the
+	// watcher when they are done.
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+
+			service := DefaultService
+			if idx > 2 {
+				service = "ensign.v1.Ensign"
+			}
+
+			for i := 0; i < idx+1; i++ {
+				if i%2 == 0 {
+					srv.SetStatus(service, StatusNotServing)
+				} else {
+					srv.SetStatus(service, StatusServing)
+				}
+			}
+
+			// Cleanup the watchers after time has passed
+			time.Sleep(15 * time.Millisecond)
+			srv.DelWatcher(fmt.Sprintf("watcher-%d", idx))
+		}(i)
+	}
+
+	wg.Wait()
+	require.Equal(t, []int{6, 6, 6, 9, 9}, counts)
+}
+
+func TestServer(t *testing.T) {
+	// Create a bufconn grpc server
+	bufnet := bufconn.New()
+	probe := &ProbeServer{}
+	srv := grpc.NewServer()
+
+	RegisterHealthServer(srv, probe)
+	go srv.Serve(bufnet.Sock())
+	defer func() {
+		srv.GracefulStop()
+		bufnet.Close()
+	}()
+
+	cc, err := bufnet.Connect(context.Background(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err, "could not connect to probe server")
+
+	var wg sync.WaitGroup
+	var ready sync.WaitGroup
+	client := NewHealthClient(cc)
+	counts := make([]int, 3)
+
+	// Create some streaming watchers
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		ready.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			var service string
+			switch i {
+			case 0:
+				service = ""
+			case 1:
+				service = DefaultService
+			case 2:
+				service = "ensign.v1.Ensign"
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			stream, err := client.Watch(ctx, &HealthCheckRequest{Service: service})
+			assert.NoError(t, err, "could not connect stream client")
+			ready.Done()
+
+			for {
+				_, err := stream.Recv()
+				assert.NoError(t, err, "could not recv message %d in thread %d", counts[i]+1, i)
+
+				counts[i]++
+				if counts[i] > 3 {
+					err = stream.CloseSend()
+					assert.NoError(t, err, "could not close send message")
+					return
+				}
+			}
+		}(i)
+	}
+
+	ready.Wait()
+
+	// Unknown service states
+	for _, service := range []string{"", DefaultService, "ensign.v1.Ensign"} {
+		rep, err := client.Check(context.Background(), &HealthCheckRequest{Service: service})
+		require.NoError(t, err)
+		require.Equal(t, HealthCheckResponse_UNKNOWN, rep.Status)
+	}
+
+	// Not Serving Service States
+	probe.NotHealthy()
+	probe.SetStatus("ensign.v1.Ensign", StatusNotServing)
+
+	for _, service := range []string{"", DefaultService, "ensign.v1.Ensign"} {
+		rep, err := client.Check(context.Background(), &HealthCheckRequest{Service: service})
+		require.NoError(t, err)
+		require.Equal(t, StatusNotServing, rep.Status)
+	}
+
+	// Serving Service States
+	probe.Healthy()
+	probe.SetStatus("ensign.v1.Ensign", StatusServing)
+
+	for _, service := range []string{"", DefaultService, "ensign.v1.Ensign"} {
+		rep, err := client.Check(context.Background(), &HealthCheckRequest{Service: service})
+		require.NoError(t, err)
+		require.Equal(t, StatusServing, rep.Status)
+	}
+
+	probe.NotHealthy()
+	probe.SetStatus("ensign.v1.Ensign", StatusNotServing)
+
+	// Check watchers
+	wg.Wait()
+	require.Equal(t, []int{4, 4, 4}, counts)
+}
