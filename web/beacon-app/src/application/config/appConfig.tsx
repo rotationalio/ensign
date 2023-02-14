b03b942f7b6b6dac6196bddddc7addee87e3b623
Benjamin Bengfort <benjamin@bengfort.com>
2023-02-14 07:06:42 -0600

    fix vite environment variables

--- web/beacon-app/src/application/config/appConfig.tsx
+++ web/beacon-app/src/application/config/appConfig.tsx
@@ -1,11 +1,19 @@
 const appConfig = {
+  nodeENV: import.meta.env.MODE,
+
+  // Backend connection information
   quaterDeckApiUrl: import.meta.env.REACT_APP_QUARTERDECK_BASE_URL || 'http://localhost:8088/v1',
   tenantApiUrl: import.meta.env.REACT_APP_TENANT_BASE_URL || 'http://localhost:8080/v1',
+
+  // Sentry information
   sentryDSN: import.meta.env.REACT_APP_SENTRY_DSN,
   sentryENV: import.meta.env.REACT_APP_SENTRY_ENVIRONMENT,
-  nodeENV: import.meta.env.NODE_ENV,
   sentryEventID: import.meta.env.REACT_APP_SENTRY_EVENT_ID,
+
+  // Google Analytics tag
   analyticsID: import.meta.env.REACT_APP_ANALYTICS_ID,
+
+  // App version information from build workflow
   version: import.meta.env.REACT_APP_VERSION_NUMBER,
   revision: import.meta.env.REACT_APP_GIT_REVISION,
 
--- web/beacon-app/src/main.tsx
+++ web/beacon-app/src/main.tsx
@@ -8,8 +8,10 @@ import { RouterProvider } from 'react-router-dom';
 import { queryClient, QueryClientProvider } from '@/application/config/react-query';
 import router from '@/application/routes/root';
 
+import { appConfig } from './application/config';
 import initSentry from './application/config/sentry';
 
+console.log('initializing beacon ui', appConfig.nodeENV, appConfig.version, appConfig.revision);
 initSentry();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
