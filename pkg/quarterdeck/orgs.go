5dba4e61fbf98554a269e1ceb114b83bf5e1ecd3
Benjamin Bengfort <benjamin@rotational.io>
2023-02-15 20:21:37 -0600

    Project One Time Access Claims (#219)
    Co-authored-by: Patrick Deziel <42919891+pdeziel@users.noreply.github.com>

--- pkg/quarterdeck/orgs.go
+++ pkg/quarterdeck/orgs.go
@@ -3,15 +3,21 @@ package quarterdeck
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/oklog/ulid/v2"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/api/v1"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/db/models"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/middleware"
+	"github.com/rotationalio/ensign/pkg/quarterdeck/permissions"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/tokens"
+	ulids "github.com/rotationalio/ensign/pkg/utils/ulid"
 	"github.com/rs/zerolog/log"
 )
+
+const OneTimeAccessDuration = 5 * time.Minute
 
 // Retrieve an organization by ID. Users are only allowed to retrieve their own
 // organization.
@@ -112,4 +118,81 @@ func (s *Server) ProjectCreate(c *gin.Context) {
 	project.Created, _ = model.GetCreated()
 	project.Modified, _ = model.GetModified()
 	c.JSON(http.StatusOK, project)
+}
+
+func (s *Server) ProjectAccess(c *gin.Context) {
+	var (
+		err     error
+		exists  bool
+		project *api.Project
+		claims  *tokens.Claims
+		creds   *api.LoginReply
+	)
+
+	// Bind the Project request to the project data structure
+	if err = c.BindJSON(&project); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("could not parse request"))
+		return
+	}
+
+	// Validate the request from the API side.
+	if err = project.Validate(); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.ErrorResponse(err))
+		return
+	}
+
+	// Fetch the user claims from the request
+	if claims, err = middleware.GetClaims(c); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("user claims unavailable"))
+		return
+	}
+
+	// Check that the orgID claim is allowed access to the project
+	model := &models.OrganizationProject{OrgID: claims.ParseOrgID(), ProjectID: project.ProjectID}
+	if exists, err = model.Exists(c.Request.Context()); !exists || err != nil {
+		if !exists {
+			c.JSON(http.StatusBadRequest, api.ErrorResponse("unknown project id"))
+			return
+		}
+
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, api.ErrorResponse("could not complete project access request"))
+		return
+	}
+
+	// Prepare the credentials for the access token to make the one time access.
+	now := time.Now()
+	ota := &tokens.Claims{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ID:        ulids.New().String(),
+			Subject:   claims.Subject,
+			IssuedAt:  jwt.NewNumericDate(now),
+			NotBefore: jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(OneTimeAccessDuration)),
+		},
+		OrgID:       claims.OrgID,
+		ProjectID:   project.ProjectID.String(),
+		Permissions: make([]string, 0, 4),
+	}
+
+	// Add only the user permissions related to topics to these claims -- whatever
+	// access to topics the user has, so to will the one time access claims.
+	for _, permission := range claims.Permissions {
+		if permissions.InGroup(permission, permissions.PrefixTopics) {
+			ota.Permissions = append(ota.Permissions, permission)
+		}
+	}
+
+	creds = &api.LoginReply{}
+	token := s.tokens.CreateToken(ota)
+	if creds.AccessToken, err = s.tokens.Sign(token); err != nil {
+		c.Error(err)
+		c.JSON(http.StatusInternalServerError, "could not complete project access request")
+		return
+	}
+
+	c.JSON(http.StatusOK, creds)
 }
