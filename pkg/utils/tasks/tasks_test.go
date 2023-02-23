e5d981dc87387047be133fc2d4c93b8411a6c11e
Benjamin Bengfort <benjamin@bengfort.com>
2023-02-23 15:14:51 -0600

    make tests more robust

--- pkg/utils/tasks/tasks_test.go
+++ pkg/utils/tasks/tasks_test.go
@@ -2,6 +2,7 @@ package tasks_test
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -10,16 +11,20 @@ import (
 )
 
 func TestTasks(t *testing.T) {
+	var wg sync.WaitGroup
 	tm := tasks.New(8, 16)
 	completed := 0
 
 	for i := 0; i < 100; i++ {
+		wg.Add(1)
 		tm.Queue(tasks.TaskFunc(func(context.Context) {
 			time.Sleep(1 * time.Millisecond)
 			completed++
+			wg.Done()
 		}))
 	}
 
+	wg.Wait()
 	tm.Stop()
 	require.Equal(t, 100, completed)
 }
