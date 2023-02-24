d8cd05a70e9540f1c6722db05a101398347e97c4
Cletus Razakou <masskoder@gmail.com>
2023-02-24 13:47:49 -0500

    refact: use profile component & useFetchMember

--- web/beacon-app/src/features/members/components/CancelModal/CancelAcctModal.stories.tsx
+++ web/beacon-app/src/features/members/components/CancelModal/CancelAcctModal.stories.tsx
@@ -1,12 +1,15 @@
 import { Meta, Story } from '@storybook/react';
 
 import CancelAcctModal from './CancelAcctModal';
-
+interface CancelAcctModalProps {
+  close: () => void;
+  isOpen: boolean;
+}
 export default {
   title: 'beacon/CancelAcctModal',
 } as Meta;
 
-const Template: Story = (args) => <CancelAcctModal {...args} />;
+const Template: Story<CancelAcctModalProps> = (args) => <CancelAcctModal {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {};
---
+++ web/beacon-app/src/features/members/hooks/useFetchMember.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
+import axiosInstance from '@/application/api/ApiService';
+import { RQK } from '@/constants';
+
+import { memberRequest } from '../api/memberApiService';
+import { MemberQuery } from '../types/memberServices';
+
+export function useFetchMember(memberID: string): MemberQuery {
+  const query = useQuery([RQK.MEMBER_DETAIL, memberID], () =>
+    memberRequest(axiosInstance)(memberID)
+  );
+
+  return {
+    getMember: query.refetch,
+    hasMemberFailed: query.isError,
+    isFetchingMember: query.isLoading,
+    member: query.data,
+    wasMemberFetched: query.isSuccess,
+    error: query.error,
+  };
+}
