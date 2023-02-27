af80d38eb65c8f5bc502a6a544edbddfac03bb8e
Benjamin Bengfort <benjamin@rotational.io>
2023-02-27 16:44:01 -0600

    Update pkg/quarterdeck/tokens/tokens_test.go
    Co-authored-by: pdamodaran <pdamodaran@users.noreply.github.com>

--- pkg/quarterdeck/tokens/tokens_test.go
+++ pkg/quarterdeck/tokens/tokens_test.go
@@ -109,6 +109,7 @@ func (s *TokenTestSuite) TestTokenManager() {
 	rc := refreshToken.Claims.(*tokens.Claims)
 	require.Equal(ac.ID, rc.ID, "access and refresh tokens must have same jid")
 	require.Equal(jwt.ClaimStrings{"http://localhost:3000", "http://localhost:3001/v1/refresh"}, rc.Audience)
+	require.NotEqual(ac.Audience, rc.Audience, "identical access token and refresh token audience")
 	require.Equal(ac.Issuer, rc.Issuer)
 	require.Equal(ac.Subject, rc.Subject)
 	require.True(rc.IssuedAt.Equal(ac.IssuedAt.Time))
