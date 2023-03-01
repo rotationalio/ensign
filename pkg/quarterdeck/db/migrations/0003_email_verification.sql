dc4c10127499640de27261b8928e20721786bf46
Patrick Deziel <deziel.patrick@gmail.com>
2023-03-01 10:44:10 -0600

    add unique index on token

--- pkg/quarterdeck/db/migrations/0003_email_verification.sql
+++ pkg/quarterdeck/db/migrations/0003_email_verification.sql
@@ -10,6 +10,9 @@ ALTER TABLE users ADD COLUMN email_verification_expires TEXT DEFAULT NULL;
 -- Token provided by the user to verify their email address.
 ALTER TABLE users ADD COLUMN email_verification_token TEXT DEFAULT NULL;
 
+-- Unique index on the token to allow for fast lookups.
+CREATE UNIQUE INDEX unique_email_verification_token ON users (email_verification_token);
+
 -- Secret key used to sign the token.
 ALTER TABLE users ADD COLUMN email_verification_secret BLOB DEFAULT NULL;
 
