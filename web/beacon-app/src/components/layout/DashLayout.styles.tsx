1730067ebf6cf2fe076b95d7c5e92d00b10c85b6
elysee15 <54010836+elysee15@users.noreply.github.com>
2023-03-02 14:04:53 +0000

    sc-14318 Review the UI & Make the sidebar responsive

--- web/beacon-app/src/components/layout/DashLayout.styles.tsx
+++ web/beacon-app/src/components/layout/DashLayout.styles.tsx
@@ -1,10 +1,7 @@
 import styled from 'styled-components';
 
-import { SIDEBAR_WIDTH, TOPBAR_HEIGHT } from '@/constants/dashLayout';
+import { TOPBAR_HEIGHT } from '@/constants/dashLayout';
 
 export const MainStyle = styled.main`
-  height: calc(100vh - ${TOPBAR_HEIGHT});
-  margin-left: ${SIDEBAR_WIDTH}px;
-  width: calc(100vw - ${SIDEBAR_WIDTH}px);
   margin-top: ${TOPBAR_HEIGHT}px;
 `;
