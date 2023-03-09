078697d76f84acc83dc9ad0cf2ef93d62118b8e2
elysee15 <elyseebleu1@gmail.com>
2023-03-09 18:34:34 +0000

    sc-14808 Beacon UI: Fix UI issues that occurred from responsiveness implementation

--- web/beacon-app/src/components/layout/MobileFooter/MobileFooter.tsx
+++ web/beacon-app/src/components/layout/MobileFooter/MobileFooter.tsx
@@ -7,7 +7,7 @@ function MobileFooter() {
     <footer className="fixed bottom-0 left-0 w-screen bg-[#1D65A6] py-1 md:hidden md:pl-[250px]">
       <ul className="flex items-center justify-around space-y-1 text-xs text-white">
         {footerItems.map((item) => (
-          <li key={item.name}>
+          <li key={`${item.name}`}>
             <Link to={item.href} target="_blank">
               {item.name}
             </Link>
