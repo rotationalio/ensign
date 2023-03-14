90a809b03ffc7b303150c8533ae61be591dbebd6
elysee15 <elyseebleu1@gmail.com>
2023-03-14 15:30:22 +0000

    sc-14844 dashboard navigation bar fixes

--- web/beacon-app/src/features/projects/routes/ProjectDetailPage.tsx
+++ web/beacon-app/src/features/projects/routes/ProjectDetailPage.tsx
@@ -1,27 +1,54 @@
-import { Heading, Loader } from '@rotational/beacon-core';
-import { lazy, Suspense, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Breadcrumbs, Heading, Loader } from '@rotational/beacon-core';
+import invariant from 'invariant';
+import { lazy, Suspense, useCallback, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import { PATH_DASHBOARD } from '@/application/routes/paths';
 import AppLayout from '@/components/layout/AppLayout';
+import BreadcrumbsIcon from '@/components/ui/Breadcrumbs/breadcrumbs-icon';
+
+import { useFetchProject } from '../hooks/useFetchProject';
+import { formatProjectData } from '../util';
 const ProjectDetail = lazy(() => import('../components/ProjectDetail'));
 const TopicTable = lazy(() => import('../components/TopicTable'));
 const APIKeysTable = lazy(() => import('../components/APIKeysTable'));
 
 const ProjectDetailPage = () => {
   const navigate = useNavigate();
-  const param = useParams<{ id: string }>() as any;
+  const param = useParams<{ id: string }>();
 
+  invariant(param.id, 'id is required');
+
+  const { project } = useFetchProject(param.id);
+  const getFormattedProjectData = formatProjectData(project);
+  console.log(project);
   useEffect(() => {
     if (!param || param.id === 'undefined' || param.id === 'null') {
       navigate(PATH_DASHBOARD.HOME);
     }
   }, [param, navigate]);
 
+  const CustomBreadcrumbs = useCallback(() => {
+    return (
+      <Breadcrumbs separator="/" className="ml-4 hidden md:block">
+        <Breadcrumbs.Item className="capitalize">
+          <Link to={PATH_DASHBOARD.HOME} className="hover:underline">
+            <BreadcrumbsIcon className="inline" /> Home
+          </Link>
+        </Breadcrumbs.Item>
+        <Breadcrumbs.Item className="!cursor-default capitalize">Projects</Breadcrumbs.Item>
+        {project?.name ? (
+          <Breadcrumbs.Item className="capitalize">{project?.name}</Breadcrumbs.Item>
+        ) : null}
+      </Breadcrumbs>
+    );
+  }, [project?.name, project?.id]);
+
   return (
-    <AppLayout>
+    <AppLayout Breadcrumbs={<CustomBreadcrumbs />}>
       <Heading as="h1" className="mb-4 text-lg font-semibold">
-        Project Detail
+        Project Details
       </Heading>
       <Suspense
         fallback={
@@ -30,7 +57,7 @@ const ProjectDetailPage = () => {
           </div>
         }
       >
-        <ProjectDetail projectID={param?.id} />
+        <ProjectDetail project={getFormattedProjectData} />
       </Suspense>
 
       <Suspense
@@ -50,7 +77,7 @@ const ProjectDetailPage = () => {
           </div>
         }
       >
-        <APIKeysTable projectID={param?.id} />
+        <APIKeysTable projectID={param.id} />
       </Suspense>
     </AppLayout>
   );
