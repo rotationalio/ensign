3b48604ead46cd1a961d80076d13167410391b05
Benjamin Bengfort <benjamin@bengfort.com>
2023-03-14 08:15:12 -0500

    Allow Multiple Package Protocol Buffer Generation

--- pkg/mimetype/generate.go
+++ pkg/mimetype/generate.go
@@ -1,3 +1,3 @@
 package mimetype
 
-//go:generate protoc -I=$GOPATH/src/github.com/rotationalio/ensign/proto --go_out=. --go_opt=module=github.com/rotationalio/ensign/pkg/mimetype mimetype/v1beta1/mimetype.proto
+//go:generate protoc -I=$GOPATH/src/github.com/rotationalio/ensign/proto --go_out=./v1beta1 --go_opt=module=github.com/rotationalio/ensign/pkg/mimetype/v1beta1 --go_opt=Mmimetype/v1beta1/mimetype.proto=github.com/rotationalio/ensign/pkg/mimetype/v1beta1;mimetype mimetype/v1beta1/mimetype.proto
