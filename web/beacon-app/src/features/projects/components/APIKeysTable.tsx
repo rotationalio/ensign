5bb8c016afd009bc54f616135256fba416dbbfb8
Cletus Razakou <masskoder@gmail.com>
2023-03-16 19:21:35 -0400

    add react toast to display modal error

--- web/beacon-app/src/features/projects/components/APIKeysTable.tsx
+++ web/beacon-app/src/features/projects/components/APIKeysTable.tsx
@@ -24,19 +24,25 @@ export const APIKeysTable = ({ projectID }: APIKeysTableProps) => {
     setIsOpenGenerateAPIKeyModal(false);
   };
 
-  const onCloseAPIKeyDataModal = () => setIsOpenAPIKeyDataModal(false);
+  const onCloseAPIKeyDataModal = () => {
+    setIsOpenAPIKeyDataModal(false);
+  };
 
   useEffect(() => {
     if (key) {
       setIsOpenAPIKeyDataModal(true);
     }
+    return () => {
+      setIsOpenAPIKeyDataModal(false);
+    };
   }, [key]);
+
   if (isFetchingApiKeys) {
     // TODO: add loading state
     return <div>Loading...</div>;
   }
 
-  if (error) {
+  if (hasApiKeysFailed) {
     <Toast
       isOpen={hasApiKeysFailed}
       variant="danger"
