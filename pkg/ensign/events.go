e4d4fb27af14c2da28d4b8851beab6bd9940cbc6
Benjamin Bengfort <benjamin@bengfort.com>
2023-03-19 18:14:51 -0500

    Restrict Publish and Subscribe Endpoints

--- pkg/ensign/events.go
+++ pkg/ensign/events.go
@@ -4,8 +4,14 @@ import (
 	"io"
 	"sync"
 
+	"github.com/oklog/ulid/v2"
 	api "github.com/rotationalio/ensign/pkg/ensign/api/v1beta1"
+	"github.com/rotationalio/ensign/pkg/ensign/contexts"
 	"github.com/rotationalio/ensign/pkg/ensign/o11y"
+	"github.com/rotationalio/ensign/pkg/ensign/store/meta"
+	"github.com/rotationalio/ensign/pkg/quarterdeck/permissions"
+	"github.com/rotationalio/ensign/pkg/utils/sentry"
+	"github.com/rotationalio/ensign/pkg/utils/ulids"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -17,9 +23,63 @@ func (s *Server) Publish(stream api.Ensign_PublishServer) (err error) {
 	o11y.OnlinePublishers.Inc()
 	defer o11y.OnlinePublishers.Dec()
 
+	// Parse the context for authentication information
+	ctx := stream.Context()
+	claims, ok := contexts.ClaimsFrom(ctx)
+	if !ok {
+		// NOTE: this should never happen because the interceptor will catch it, but
+		// this check prevents nil panics and guards against regressions.
+		sentry.Fatal(ctx).Msg("no claims available in publish stream")
+		return status.Error(codes.Unauthenticated, "missing credentials")
+	}
+
+	// Verify that the user has permissions to publish.
+	if !claims.HasPermission(permissions.Publisher) {
+		log.Warn().Msg("attempt to open publisher stream without publisher permission")
+		return status.Error(codes.Unauthenticated, "not authorized to perform this action")
+	}
+
+	var projectID ulid.ULID
+	if projectID, err = ulids.Parse(claims.ProjectID); err != nil || ulids.IsZero(projectID) {
+		sentry.Warn(ctx).Err(err).Msg("could not parse projectID from claims")
+		return status.Error(codes.Unauthenticated, "not authorized to perform this action")
+	}
+
+	// Get the topic IDs this user is allowed to publish to
+	// TODO: make this more efficient and globalize to prevent inconsistencies
+	allowedTopics := make(map[string]struct{})
+	topics := s.meta.ListTopics(projectID)
+
+	for topics.Next() {
+		// Get and parse the key for the topic ID
+		var key meta.ObjectKey
+		copy(key[:], topics.Key())
+
+		// Parse the ULID:
+		var topicID ulid.ULID
+		if topicID, err = key.ObjectID(); err != nil {
+			sentry.Error(ctx).Err(err).Msg("could not parse topic id")
+			topics.Release()
+			return status.Error(codes.Internal, "could not open publisher stream")
+		}
+
+		allowedTopics[topicID.String()] = struct{}{}
+	}
+
+	if err := topics.Error(); err != nil {
+		sentry.Warn(ctx).Err(err).Msg("could not fetch topics")
+		topics.Release()
+		return status.Error(codes.Internal, "could not open publisher stream")
+	}
+	topics.Release()
+
+	if len(allowedTopics) == 0 {
+		log.Warn().Msg("publisher created with no topics")
+		return status.Error(codes.FailedPrecondition, "no topics available")
+	}
+
 	// Set up the stream handlers
 	nEvents := uint64(0)
-	ctx := stream.Context()
 	events := make(chan *api.Event, BufferSize)
 
 	var wg sync.WaitGroup
@@ -30,6 +90,51 @@ func (s *Server) Publish(stream api.Ensign_PublishServer) (err error) {
 	go func(events <-chan *api.Event) {
 		defer wg.Done()
 		for event := range events {
+			// Verify the event has a topic associated with it
+			if event.TopicId == "" {
+				// TODO: should we error here and close the stream?
+				log.Warn().Msg("event published without topic id")
+
+				// Send the nack back to the user
+				stream.Send(&api.Publication{
+					// TODO: what are the nack error codes?
+					Embed: &api.Publication_Nack{
+						Nack: &api.Nack{
+							Id:    event.Id,
+							Code:  uint32(12),
+							Error: "event requires topic id",
+						},
+					},
+				})
+
+				// Continue processing
+				continue
+			}
+
+			// Verify the event is in a topic that the user is allowed to publish to
+			// TODO: this won't allow topics that were created after the stream was
+			// created but are still valid. Need to unify the allowed mechanism into
+			// a global topic handler check rather than in a per-stream check.
+			if _, ok := allowedTopics[event.TopicId]; !ok {
+				// TODO: should we error here and close the stream?
+				log.Warn().Msg("event published to topic that is not allowed")
+
+				// Send the nack back to the user
+				stream.Send(&api.Publication{
+					// TODO: what are the nack error codes?
+					Embed: &api.Publication_Nack{
+						Nack: &api.Nack{
+							Id:    event.Id,
+							Code:  uint32(99),
+							Error: "unknown topic id",
+						},
+					},
+				})
+
+				// Continue processing
+				continue
+			}
+
 			// Push event on to the primary buffer
 			s.pubsub.Publish(event)
 
