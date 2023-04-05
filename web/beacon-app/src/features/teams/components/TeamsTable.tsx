8e7ce46ee3e2a02b84aae0c056de6baf640642d2
daniellemaxwell <danielle@rotational.io>
2023-04-05 16:45:39 -0400

    Adds status indicator and updates formatDate

--- web/beacon-app/src/features/teams/components/TeamsTable.tsx
+++ web/beacon-app/src/features/teams/components/TeamsTable.tsx
@@ -1,12 +1,15 @@
 import { Loader, Table, Toast } from '@rotational/beacon-core';
 
+import ConfirmedIndicator from '@/components/icons/confirmedIndicator';
+import PendingIndicator from '@/components/icons/pendingIndicator';
 import { useFetchMembers } from '@/features/members/hooks/useFetchMembers';
+import { formatDate } from '@/utils/formatDate';
 
 import { getMembers } from '../util';
 
 function TeamsTable() {
   const { members, isFetchingMembers, hasMembersFailed, error } = useFetchMembers();
-  console.log(members);
+
   if (isFetchingMembers) {
     return <Loader />;
   }
@@ -30,14 +33,39 @@ function TeamsTable() {
           { Header: 'Name', accessor: 'name' },
           { Header: 'Email Address', accessor: 'email' },
           { Header: 'Role', accessor: 'role' },
-          { Header: 'Status', accessor: 'status' },
-          { Header: 'Last Activity', accessor: 'last_activity' },
-          { Header: 'Joined Date', accessor: 'date_added' },
+          {
+            Header: 'Status',
+            accessor: (m: any) => {
+              return (
+                <div className="flex items-center">
+                  {m.status === 'Confirmed' && <ConfirmedIndicator />}
+                  {m.status === 'Pending' && <PendingIndicator />}
+                  <span className="pl-1">{m.status}</span>
+                </div>
+              );
+            },
+          },
+          {
+            Header: 'Last Activity',
+            accessor: (date: any) => {
+              return formatDate(new Date(date.last_activity));
+            },
+          },
+          {
+            Header: 'Joined Date',
+            accessor: (date: any) => {
+              return formatDate(new Date(date.date_added));
+            },
+          },
           {
             // TODO: Make actions viewable only to members with owner and admin permission
             Header: 'Actions',
             Cell: () => {
-              return <div>&hellip;</div>;
+              return (
+                <button type="button" className="mb-2 text-2xl font-bold">
+                  &hellip;
+                </button>
+              );
             },
           },
         ]}
