37fd2d893712e37c6b6492f17eb5c1ec0cb80257
Cletus Razakou <masskoder@gmail.com>
2023-04-06 18:47:59 -0400

    fix: add nullsafety of member list (#379)

--- web/beacon-app/src/features/teams/util.ts
+++ web/beacon-app/src/features/teams/util.ts
@@ -3,33 +3,33 @@ export const formatMemberData = (data: any) => {
   return [
     {
       label: 'Name',
-      value: data.name,
+      value: data?.name,
     },
     {
       label: 'Email',
-      value: data.email,
+      value: data?.email,
     },
     {
       label: 'Role',
-      value: data.role,
+      value: data?.role,
     },
     {
       label: 'Status',
-      value: data.status,
+      value: data?.status,
     },
     {
       label: 'Last Activity',
-      value: data.last_activity,
+      value: data?.last_activity,
     },
     {
       label: 'Joined Date',
-      value: data.date_added,
+      value: data?.date_added,
     },
   ];
 };
 
 export const getMembers = (members: any) => {
-  if (!members?.members || members?.members.length === 0) return [];
+  if (!members?.members || members?.members?.length === 0) return [];
   return Object.keys(members?.members).map((m) => {
     const { name, email, role, status, last_activity, date_added } = members.members[m];
     return { name, email, role, status, last_activity, date_added };
