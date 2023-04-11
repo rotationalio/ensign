f960e1a5fda7b99e5d447eecae277b1c90ce4af3
Cletus Razakou <masskoder@gmail.com>
2023-04-11 11:39:58 -0400

    fix toast close button issue

--- web/beacon-app/src/features/auth/routes/RegistrationPage.tsx
+++ web/beacon-app/src/features/auth/routes/RegistrationPage.tsx
@@ -1,6 +1,5 @@
+import { Toast } from '@rotational/beacon-core';
 import { FormikHelpers } from 'formik';
-import { useEffect } from 'react';
-import toast from 'react-hot-toast';
 import { Link, useNavigate } from 'react-router-dom';
 
 // import { slugify } from '@/utils/slugifyDomain';
@@ -33,14 +32,17 @@ export function Registration() {
       },
     });
   };
-  useEffect(() => {
-    if (register.hasAccountFailed) {
-      toast.error((register.error as any)?.response?.data?.error);
-    }
-  }, [register.hasAccountFailed, register.error]);
 
   return (
     <>
+      {register.hasAccountFailed && (
+        <Toast
+          isOpen={register.hasAccountFailed}
+          variant="danger"
+          description={(register.error as any)?.response?.data?.error}
+        />
+      )}
+
       <div className="flex flex-col gap-4 px-4 py-8 text-sm sm:p-8 md:flex-row md:p-16 xl:text-base">
         <div className="space-y-4 rounded-md border border-[#1D65A6] bg-[#1D65A6] p-4 text-white sm:p-8 md:w-2/6">
           <h1 className="text-center font-bold">
