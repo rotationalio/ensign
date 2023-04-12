cc2bd5ecdb4cc83a0c28cefd7240c189c5d40b0b
Danielle <danielle@rotational.io>
2023-04-12 10:11:27 -0400

    Sc 16006/registration (#390)

--- pkg/tenant/auth_test.go
+++ pkg/tenant/auth_test.go
@@ -34,10 +34,42 @@ func (s *tenantTestSuite) TestRegister() {
 		return &pb.PutReply{}, nil
 	}
 
+	orgID := ulid.MustParse("02GQ38J5YWH4DCYJ6CZ2P5DA35")
+	id := ulid.MustParse("01GQ38J5YWH4DCYJ6CZ2P5DA2G")
+
+	members := []*db.Member{
+		{
+			OrgID:  orgID,
+			ID:     id,
+			Email:  "leopold.wentzel@gmail.com",
+			Name:   "Leopold Wentzel",
+			Role:   perms.RoleAdmin,
+			Status: db.MemberStatusPending,
+		},
+	}
+
+	// Connect to trtl mock and call OnCursor to loop through members in the database.
+	trtl.OnCursor = func(in *pb.CursorRequest, stream pb.Trtl_CursorServer) error {
+		if !bytes.Equal(in.Prefix, orgID[:]) {
+			return status.Error(codes.FailedPrecondition, "unexpected cursor request")
+		}
+
+		for _, member := range members {
+			data, err := member.MarshalValue()
+			require.NoError(err, "could not marshal data")
+			stream.Send(&pb.KVPair{
+				Key:       []byte(member.Email),
+				Value:     data,
+				Namespace: db.MembersNamespace,
+			})
+		}
+		return nil
+	}
+
 	// Create initial fixtures
 	reply := &qd.RegisterReply{
-		ID:      ulid.MustParse("01GQ38J5YWH4DCYJ6CZ2P5DA2G"),
-		OrgID:   ulid.MustParse("02GQ38J5YWH4DCYJ6CZ2P5DA35"),
+		ID:      id,
+		OrgID:   orgID,
 		Email:   "leopold.wentzel@gmail.com",
 		Message: "Welcome to Ensign!",
 		Role:    perms.RoleAdmin,
@@ -135,12 +167,17 @@ func (s *tenantTestSuite) TestRegister() {
 	err = s.client.Register(ctx, req)
 	require.NoError(err, "could not complete registration")
 
+	// Test registration with an invite token.
+	req.InviteToken = "pUqQaDxWrqSGZzkxFDYNfCMSMlB9gpcfzorN8DsdjIA"
+	err = s.client.Register(ctx, req)
+	require.NoError(err, "could not complete registration with invite token")
+
 	// Test that a tenant, member, and project were created without error
 	s.StopTasks()
-	require.Equal(7, trtl.Calls[trtlmock.PutRPC], "expected 7 Put calls to trtl for two puts for each tenant, member, and project (store and org index) and one for object_keys.")
+	require.Equal(8, trtl.Calls[trtlmock.PutRPC], "expected 8 Put calls to trtl, two puts for each tenant, member, and project (store and org index), one for object_keys for new users, and one for a new invited user.")
 	require.Equal(0, trtl.Calls[trtlmock.GetRPC], "expected no gets on register")
 	require.Equal(0, trtl.Calls[trtlmock.DeleteRPC], "expected no deletes on register")
-	require.Equal(0, trtl.Calls[trtlmock.CursorRPC], "expected no cursors on register")
+	require.Equal(1, trtl.Calls[trtlmock.CursorRPC], "expected 1 cursor call on register")
 
 	// Register method should handle errors from Quarterdeck
 	s.quarterdeck.OnRegister(mock.UseError(http.StatusBadRequest, "password too weak"))
@@ -227,6 +264,7 @@ func (s *tenantTestSuite) TestLogin() {
 	require.NoError(err, "could not complete login")
 	require.Equal(expected, rep, "unexpected login reply")
 
+	// Set invite token and test login.
 	req.InviteToken = "pUqQaDxWrqSGZzkxFDYNfCMSMlB9gpcfzorN8DsdjIA"
 	rep, err = s.client.Login(ctx, req)
 	require.NoError(err, "could not complete login")
