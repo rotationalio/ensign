dd303a09ce0d0db2ceeaba4aa947372e68193a06
Cletus Razakou <masskoder@gmail.com>
2023-04-13 14:00:18 -0400

    clean up

--- web/beacon-app/src/features/teams/loaders/inviteTeamMember.ts
+++ web/beacon-app/src/features/teams/loaders/inviteTeamMember.ts
@@ -1,17 +1,12 @@
 import { LoaderFunctionArgs } from 'react-router-dom';
 
 import axiosInstance from '@/application/api/ApiService';
-import { setCookie } from '@/utils/cookies';
 
 import { getInviteTeamMemberRequest } from '../api/getInviteTeamMemberRequest';
 
 export const inviteTeamMemberLoader = async ({ request }: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const token = url.searchParams.get('token') || '';
-
-  if (token) {
-    setCookie('invite_token', token);
-  }
 
   const response = await getInviteTeamMemberRequest(axiosInstance)(token);
   return response;
