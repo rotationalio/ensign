21c077f6a2e93a24d6cad6b04b57084c61303517
Cletus Razakou <masskoder@gmail.com>
2023-04-20 16:36:49 -0400

    fix password checker issue

--- web/beacon-app/src/features/auth/components/Register/schemas/registrationFormValidation.ts
+++ web/beacon-app/src/features/auth/components/Register/schemas/registrationFormValidation.ts
@@ -5,7 +5,17 @@ import { NewUserAccount } from '../../../types/RegisterService';
 const validationSchema = Yup.object().shape({
   name: Yup.string().required('The name is required.'),
   email: Yup.string().email().required('The email address is required.'),
-  password: Yup.string().required('The password is required.'),
+  password: Yup.string()
+    .required('The password is required.')
+    .matches(/^(?=.*[a-z])/, 'The password must contain at least one lowercase letter.')
+    .matches(/^(?=.*[A-Z])/, 'The password must contain at least one uppercase letter.')
+    .matches(/^(?=.*[0-9])/, 'The password must contain at least one number.')
+    .matches(
+      /^(?=.*[!/[@#$%^&*+,-./:;<=>?^_`{|}~])/,
+      'The password must contain at least one special character.'
+    )
+    .matches(/^(?=.{12,})/, 'The password must be at least 12 characters long.'),
+
   pwcheck: Yup.string()
     .oneOf([Yup.ref('password'), null], 'The passwords must match.')
     .required('The confirm password is required.'),
