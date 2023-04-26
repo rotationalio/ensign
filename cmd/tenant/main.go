aee38c8fbf93ec3d537dc1e7cb3e8ffebb553944
Patrick Deziel <deziel.patrick@gmail.com>
2023-04-26 10:49:48 -0500

    org index migration

--- cmd/tenant/main.go
+++ cmd/tenant/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/joho/godotenv"
+	"github.com/oklog/ulid/v2"
 	"github.com/rotationalio/ensign/pkg"
 	"github.com/rotationalio/ensign/pkg/tenant"
 	"github.com/rotationalio/ensign/pkg/tenant/config"
+	"github.com/rotationalio/ensign/pkg/tenant/db"
+	"github.com/rotationalio/ensign/pkg/utils/pagination"
+	"github.com/rotationalio/ensign/pkg/utils/ulids"
+	trtl "github.com/trisacrypto/directory/pkg/trtl/pb/v1"
 	"github.com/urfave/cli/v2"
 )
 
@@ -20,7 +29,6 @@ func main() {
 	app.Name = "tenant"
 	app.Version = pkg.Version()
 	app.Usage = "run and manage a tenant server"
-	app.Flags = []cli.Flag{}
 	app.Commands = []*cli.Command{
 		{
 			Name:     "serve",
@@ -28,6 +36,33 @@ func main() {
 			Category: "server",
 			Action:   serve,
 			Flags:    []cli.Flag{},
+		},
+		{
+			Name:     "db:list",
+			Usage:    "list all keys in a tenant namespace",
+			Category: "client",
+			Action:   listKeys,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "namespace",
+					Aliases:  []string{"n"},
+					Usage:    "namespace to list keys for",
+					Required: true,
+				},
+			},
+		},
+		{
+			Name:     "db:migrate:orgs",
+			Usage:    "migrate all missing organization keys",
+			Category: "client",
+			Action:   migrateKeys,
+			Flags: []cli.Flag{
+				&cli.BoolFlag{
+					Name:    "dry-run",
+					Aliases: []string{"d"},
+					Usage:   "print what would be migrated without actually migrating",
+				},
+			},
 		},
 	}
 
@@ -53,6 +88,224 @@ func serve(c *cli.Context) (err error) {
 
 	if err = srv.Serve(); err != nil {
 		return cli.Exit(err, 1)
+	}
+
+	return nil
+}
+
+//===========================================================================
+// Client Commands
+//===========================================================================
+
+func migrateKeys(c *cli.Context) (err error) {
+	dry := c.Bool("dry-run")
+
+	var conf config.Config
+	if conf, err = config.New(); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	if err = db.Connect(conf.Database); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Fetch all current organization keys
+	orgKeys := make(map[ulid.ULID]struct{})
+	fetchKeys := func(item *trtl.KVPair) error {
+		id, err := ulids.Parse(item.Key)
+		if err != nil {
+			return cli.Exit(err, 1)
+		}
+		orgKeys[id] = struct{}{}
+		return nil
+	}
+
+	if _, err = db.List(ctx, nil, nil, db.OrganizationNamespace, fetchKeys, nil); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	// Fetch all tenants that do not have an organization key
+	missingTenants := make(map[*db.Key]struct{})
+	fetchTenants := func(item *trtl.KVPair) error {
+		key := &db.Key{}
+		if err = key.UnmarshalValue(item.Key); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		var object ulid.ULID
+		if object, err = key.ObjectID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		if _, ok := orgKeys[object]; !ok {
+			missingTenants[key] = struct{}{}
+		}
+
+		return nil
+	}
+
+	if _, err = db.List(ctx, nil, nil, db.TenantNamespace, fetchTenants, nil); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	// Fetch all projects that do not have an organization key
+	missingProjects := make(map[db.Key]struct{})
+	fetchProjects := func(item *trtl.KVPair) error {
+		key := &db.Key{}
+		if err = key.UnmarshalValue(item.Key); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		var object ulid.ULID
+		if object, err = key.ObjectID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		if _, ok := orgKeys[object]; !ok {
+			// Projects are stored by tenantID:projectID, however we need the orgID for the index
+			project := &db.Project{}
+			if err = project.UnmarshalValue(item.Value); err != nil {
+				return cli.Exit(err, 1)
+			}
+
+			var orgKey db.Key
+			if orgKey, err = db.CreateKey(project.OrgID, project.ID); err != nil {
+				return cli.Exit(err, 1)
+			}
+
+			missingProjects[orgKey] = struct{}{}
+		}
+
+		return nil
+	}
+
+	if _, err = db.List(ctx, nil, nil, db.ProjectNamespace, fetchProjects, nil); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	// Create organization keys for all missing tenants and projects
+	var migratedTenants, migratedProjects int
+	var errs *multierror.Error
+	for tenant := range missingTenants {
+		var id ulid.ULID
+		if id, err = tenant.ObjectID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		var orgID ulid.ULID
+		if orgID, err = tenant.ParentID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		if dry {
+			fmt.Println("Missing org key for tenant", id.String())
+			continue
+		}
+
+		if err = db.PutOrgIndex(ctx, id, orgID); err != nil {
+			errs = multierror.Append(errs, err)
+			continue
+		}
+
+		migratedTenants++
+	}
+
+	for project := range missingProjects {
+		var id ulid.ULID
+		if id, err = project.ObjectID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		var orgID ulid.ULID
+		if orgID, err = project.ParentID(); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		if dry {
+			fmt.Println("Missing org key for project", id.String())
+			continue
+		}
+
+		if err = db.PutOrgIndex(ctx, id, orgID); err != nil {
+			errs = multierror.Append(errs, err)
+			continue
+		}
+
+		migratedProjects++
+	}
+
+	if errs != nil {
+		fmt.Println(errs.Error())
+	}
+
+	if dry {
+		fmt.Printf("%d tenants and %d projects would be migrated\n", len(missingTenants), len(missingProjects))
+	} else {
+		fmt.Println("Migrated", migratedTenants, "tenants and", migratedProjects, "projects")
+	}
+
+	return nil
+}
+
+func listKeys(c *cli.Context) (err error) {
+	var conf config.Config
+	if conf, err = config.New(); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	if err = db.Connect(conf.Database); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	// Print keys in the namespace
+	onListItem := func(item *trtl.KVPair) error {
+		switch len(item.Key) {
+		case 16:
+			id, err := ulids.Parse(item.Key)
+			if err != nil {
+				return cli.Exit(err, 1)
+			}
+			fmt.Println(id.String())
+		case 32:
+			key := &db.Key{}
+			if err = key.UnmarshalValue(item.Key); err != nil {
+				return cli.Exit(err, 1)
+			}
+
+			var parent ulid.ULID
+			if parent, err = key.ParentID(); err != nil {
+				return cli.Exit(err, 1)
+			}
+
+			var object ulid.ULID
+			if object, err = key.ObjectID(); err != nil {
+				return cli.Exit(err, 1)
+			}
+
+			fmt.Println(parent.String(), object.String())
+		default:
+			return cli.Exit(fmt.Errorf("unexpected key length: %d", len(item.Key)), 1)
+		}
+
+		return nil
+	}
+
+	// Get all the keys in the namespace
+	var next *pagination.Cursor
+	for {
+		if next, err = db.List(ctx, nil, nil, c.String("namespace"), onListItem, next); err != nil {
+			return cli.Exit(err, 1)
+		}
+
+		if next == nil {
+			break
+		}
 	}
 
 	return nil
