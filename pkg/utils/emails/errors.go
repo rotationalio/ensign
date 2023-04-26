2d9039dec29f94c4f96f73f99d00fab791c895b7
Benjamin Bengfort <benjamin@rotational.io>
2023-04-26 15:58:13 -0500

    Parse Config Emails (#447)

--- pkg/utils/emails/errors.go
+++ pkg/utils/emails/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrMissingSubject   = errors.New("missing email subject")
 	ErrMissingSender    = errors.New("missing email sender")
 	ErrMissingRecipient = errors.New("missing email recipient")
+	ErrUnparsable       = errors.New("could not parse email address")
 )
