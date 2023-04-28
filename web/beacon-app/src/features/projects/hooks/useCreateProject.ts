d274af80005fe14a859c7ab7aa131494f432c6d9
Cletus Razakou <masskoder@gmail.com>
2023-04-28 18:00:52 -0400

    Sc-15723 : implement create project feature & minor improvement (#455)

---
+++ web/beacon-app/src/features/projects/hooks/useCreateProject.ts
@@ -0,0 +1,25 @@
+import { useMutation } from '@tanstack/react-query';
+
+import axiosInstance from '@/application/api/ApiService';
+import { queryClient } from '@/application/config/react-query';
+import { RQK } from '@/constants';
+
+import { createProjectAPI } from '../api/createProjectAPI';
+import { ProjectMutation } from '../types/createProjectService';
+export function useCreateProject(): ProjectMutation {
+  const mutation = useMutation(createProjectAPI(axiosInstance), {
+    retry: 0,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [RQK.PROJECTS] });
+    },
+  });
+  return {
+    createNewProject: mutation.mutate,
+    reset: mutation.reset,
+    project: mutation.data as ProjectMutation['project'],
+    hasProjectFailed: mutation.isError,
+    wasProjectCreated: mutation.isSuccess,
+    isCreatingProject: mutation.isLoading,
+    error: mutation.error,
+  };
+}
