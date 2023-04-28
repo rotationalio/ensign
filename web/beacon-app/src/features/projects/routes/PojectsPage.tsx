b6effaadad6a03a53427278f004eea486d2b410b
elysee15 <elyseebleu1@gmail.com>
2023-04-28 16:02:42 +0000

    sc-15717 Quick view component

--- web/beacon-app/src/features/projects/routes/PojectsPage.tsx
+++ web/beacon-app/src/features/projects/routes/PojectsPage.tsx
@@ -1,10 +1,15 @@
+import { useLoaderData } from 'react-router-dom';
+
+import { QuickView } from '@/components/common/QuickView';
 import AppLayout from '@/components/layout/AppLayout';
 
 import ProjectList from '../components/ProjectList';
 
 function PojectsPage() {
+  const loaderData = useLoaderData() as any;
   return (
     <AppLayout>
+      <QuickView data={loaderData?.projectStats} />
       <ProjectList />
     </AppLayout>
   );
