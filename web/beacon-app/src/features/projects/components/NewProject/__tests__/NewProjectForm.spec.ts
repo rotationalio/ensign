dee89350d7661aaa82393962930f575cd87f1e18
Cletus Razakou <masskoder@gmail.com>
2023-05-02 10:29:00 -0400

    remove unused declaration

--- web/beacon-app/src/features/projects/components/NewProject/__tests__/NewProjectForm.spec.ts
+++ web/beacon-app/src/features/projects/components/NewProject/__tests__/NewProjectForm.spec.ts
@@ -1,13 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import React from 'react';
 import { vi } from 'vitest';
-import { customRender } from '../../../../../utils/test-utils';
-import NewProjectForm from '../NewProjectForm';
-
-// mock t and Trans
 
 describe('NewProjectForm', () => {
-  it('should render', () => {
-    const handleSubmit = vi.fn();
-  });
+  it('should render', () => {});
 });
