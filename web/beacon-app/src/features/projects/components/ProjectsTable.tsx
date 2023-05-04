e73d55c9ae5b5946ef11fc6e81e8c1ed577d9e0e
Cletus Razakou <masskoder@gmail.com>
2023-05-04 06:55:45 -0400

    feat: make project row clickable

--- web/beacon-app/src/features/projects/components/ProjectsTable.tsx
+++ web/beacon-app/src/features/projects/components/ProjectsTable.tsx
@@ -1,8 +1,11 @@
 import { t } from '@lingui/macro';
 import { Table } from '@rotational/beacon-core';
-import { ErrorBoundary } from '@sentry/react';
-import { useCallback } from 'react';
+// import { ErrorBoundary } from '@sentry/react';
+import React, { useCallback, useMemo } from 'react';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
+import { PATH_DASHBOARD } from '@/application/routes/paths';
 import { formatDate } from '@/utils/formatDate';
 
 import { Project } from '../types/Project';
@@ -11,36 +14,45 @@ type ProjectTableProps = {
   projects: Project[];
 };
 
-const initialColumns: any = [
-  { Header: t`Project Name`, accessor: 'name' },
-  {
-    Header: t`Description`,
-    accessor: (p: Project) => {
-      const description = p?.description;
-      if (!description) {
-        return '---';
-      }
-      // cut off description at 100 characters
-      return description?.length > 100 ? `${description?.slice(0, 100)}...` : description || '---';
-    },
-  },
-  {
-    Header: 'Status',
-    accessor: (p: Project) => {
-      const status = p?.status;
-      return status || '---';
-    },
-  },
-  {
-    Header: t`Date Created`,
-    accessor: (date: any) => {
-      return formatDate(new Date(date?.created));
-    },
-  },
-  // { Header: 'Actions', accessor: 'actions' },
-];
+const ProjectsTable: React.FC<ProjectTableProps> = ({ projects }) => {
+  const navigate = useNavigate();
+  const initialColumns = useMemo(
+    () => [
+      { Header: t`Project ID`, accessor: 'id' },
+      { Header: t`Project Name`, accessor: 'name' },
+      {
+        Header: t`Description`,
+        accessor: (p: Project) => {
+          const description = p?.description;
+          if (!description) {
+            return '---';
+          }
+          // cut off description at 100 characters
+          return description?.length > 100
+            ? `${description?.slice(0, 100)}...`
+            : description || '---';
+        },
+      },
+      {
+        Header: 'Status',
+        accessor: (p: Project) => {
+          const status = p?.status;
+          return status || '---';
+        },
+      },
+      {
+        Header: t`Date Created`,
+        accessor: (date: any) => {
+          return formatDate(new Date(date?.created));
+        },
+      },
+      // { Header: 'Actions', accessor: 'actions' },
+    ],
+    []
+  ) as any;
 
-function ProjectsTable({ projects }: ProjectTableProps) {
+  const initialState = { hiddenColumns: ['id'] };
+
   const handleRenameProjectClick = (projectId: string) => {
     console.log('clicked!', projectId);
   };
@@ -57,9 +69,19 @@ function ProjectsTable({ projects }: ProjectTableProps) {
     }));
   }, []);
 
+  const handleRedirection = (row: any) => {
+    if (!row?.values?.id) {
+      // add toast here
+      toast.error(
+        t`Sorry, we are having trouble redirecting you to your project. Please try again.`
+      );
+    }
+    navigate(`${PATH_DASHBOARD.PROJECTS}/${row?.values?.id}`);
+  };
+
   return (
     <div className="mx-4">
-      <ErrorBoundary
+      {/* <ErrorBoundary
         fallback={
           <div className="item-center my-auto flex w-full text-center font-bold text-danger-500">
             <p>
@@ -68,11 +90,19 @@ function ProjectsTable({ projects }: ProjectTableProps) {
             </p>
           </div>
         }
-      >
-        <Table trClassName="text-sm" columns={initialColumns} data={getProjects(projects) || []} />
-      </ErrorBoundary>
+      > */}
+      <Table
+        trClassName="text-sm"
+        columns={initialColumns}
+        initialState={initialState}
+        data={getProjects(projects) || []}
+        onRowClick={(row: any) => {
+          handleRedirection(row);
+        }}
+      />
+      {/* </ErrorBoundary> */}
     </div>
   );
-}
+};
 
 export default ProjectsTable;
