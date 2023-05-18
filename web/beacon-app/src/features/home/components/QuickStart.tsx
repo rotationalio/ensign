18dc012a0f6be113853f8d2665aee81168b9bb1f
daniellemaxwell <danielle@rotational.io>
2023-05-18 14:22:51 -0400

    Adjusts otter position

--- web/beacon-app/src/features/home/components/QuickStart.tsx
+++ web/beacon-app/src/features/home/components/QuickStart.tsx
@@ -6,7 +6,7 @@ import AccessDocumentationStep from './AccessDocumentationStep';
 export default function QuickStart() {
   return (
     <div className="relative w-full space-y-10">
-      <OtterLookingDown className="2xl:right-18 hidden md:absolute md:-top-[215px] md:left-8 md:block md:scale-[.65] xl:left-3/4" />
+      <OtterLookingDown className="hidden md:absolute md:-top-[215px] md:-right-20 md:block md:scale-[.65] " />
       <SetupNewProject />
       <AccessDocumentationStep />
     </div>
