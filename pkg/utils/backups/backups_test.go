7314cbefa68aebc9771f1777da329de46791907f
Benjamin Bengfort <benjamin@rotational.io>
2023-05-29 18:36:32 -0500

    Finalize Backups Tests (#530)

--- pkg/utils/backups/backups_test.go
+++ pkg/utils/backups/backups_test.go
@@ -1,14 +1,29 @@
 package backups_test
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"crypto/sha512"
 	"errors"
+	"fmt"
+	"io"
+	"math/rand"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/rotationalio/ensign/pkg/utils/backups"
 	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+const (
+	MaxBackupRecords = 1177
 )
 
 type MockBackup struct {
@@ -85,4 +100,134 @@ func TestCanMkdTemp(t *testing.T) {
 	require.NoError(t, err, "could not create tempdir")
 	require.DirExists(t, dir, "expected tmp dir to exist")
 	require.True(t, strings.HasPrefix(dir, "./testdata"))
+}
+
+// A helper function that checks to make sure the fixture at the path exists; if it
+// doesn't then the createFixture function is run and any errors returned.
+func checkFixture(path string, createFixture func(path string) error) (err error) {
+	if _, err = os.Stat(path); os.IsNotExist(err) {
+		return createFixture(path)
+	}
+	return nil
+}
+
+// A helper function to compute the SHA512 hash of a file for comparison purposes.
+func fileHash(path string) (_ []byte, err error) {
+	var f *os.File
+	if f, err = os.Open(path); err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	hash := sha512.New()
+	if _, err = io.Copy(hash, f); err != nil {
+		return nil, err
+	}
+
+	return hash.Sum(nil), nil
+}
+
+// Extract a gzip file to the specified destination directory.
+func extract(file, destDir string, skipHidden bool) (root string, err error) {
+	var (
+		f  *os.File
+		gr *gzip.Reader
+	)
+
+	// Read the gzip file.
+	if f, err = os.Open(file); err != nil {
+		return "", err
+	}
+	defer f.Close()
+	if gr, err = gzip.NewReader(f); err != nil {
+		return "", err
+	}
+	defer gr.Close()
+
+	// Write the contents to the temporary directory.
+	tr := tar.NewReader(gr)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return "", err
+		}
+		switch hdr.Typeflag {
+		case tar.TypeDir:
+			if err = os.MkdirAll(filepath.Join(destDir, hdr.Name), os.FileMode(hdr.Mode)); err != nil {
+				return "", err
+			}
+			if root == "" {
+				root = filepath.Join(destDir, hdr.Name)
+			}
+		case tar.TypeReg:
+			var reg *os.File
+			if skipHidden && hdr.Name[0] == '.' {
+				// Skip hidden files if requested.
+				continue
+			}
+			if reg, err = os.Create(filepath.Join(destDir, hdr.Name)); err != nil {
+				return "", err
+			}
+			if _, err = io.Copy(reg, tr); err != nil {
+				reg.Close()
+				return "", err
+			}
+			reg.Close()
+		default:
+			return "", fmt.Errorf("extracting %s: unknown type flag: %c", hdr.Name, hdr.Typeflag)
+		}
+	}
+	return root, nil
+}
+
+// Archive a directory to the specified gzip file
+func archive(dir, file string) (err error) {
+	var (
+		f *os.File
+	)
+	// Create a gzip file.
+	if f, err = os.Create(file); err != nil {
+		return err
+	}
+	defer f.Close()
+	gw := gzip.NewWriter(f)
+	defer gw.Close()
+
+	// Create a tar file.
+	tw := tar.NewWriter(gw)
+	defer tw.Close()
+
+	// Write the DB to the tar file.
+	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		var hdr *tar.Header
+		if hdr, err = tar.FileInfoHeader(info, ""); err != nil {
+			return err
+		}
+		hdr.Name = path[len(dir):]
+		if err = tw.WriteHeader(hdr); err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		var tmp *os.File
+		if tmp, err = os.Open(path); err != nil {
+			return err
+		}
+		defer tmp.Close()
+		if _, err = io.Copy(tw, tmp); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
