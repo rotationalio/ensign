5e8aff6b75ecf1f71da63f92912462ba26e3e451
Danielle <danielle@rotational.io>
2023-06-09 13:47:20 -0400

    Adds refresh button to member page (#535)
    Co-authored-by: Cletus Razakou <masskoder@gmail.com>

--- web/beacon-app/src/features/teams/routes/TeamsPage.tsx
+++ web/beacon-app/src/features/teams/routes/TeamsPage.tsx
@@ -2,9 +2,11 @@ import { Trans } from '@lingui/macro';
 import { Button, Heading } from '@rotational/beacon-core';
 import { useState } from 'react';
 
+import RefreshIcon from '@/components/icons/refresh';
 import Union from '@/components/icons/union';
 import AppLayout from '@/components/layout/AppLayout';
 import { USER_PERMISSIONS } from '@/constants/rolesAndStatus';
+import { useFetchMembers } from '@/features/members/hooks/useFetchMembers';
 import { usePermissions } from '@/hooks/usePermissions';
 
 import AddNewMemberModal from '../components/AddNewMember/AddNewMemberModal';
@@ -16,6 +18,12 @@ export function TeamsPage() {
   const onOpen = () => setIsModalOpened(true);
 
   const hasPermissions = hasPermission(USER_PERMISSIONS.COLLABORATORS_ADD);
+
+  const { getMembers, isFetchingMembers } = useFetchMembers();
+
+  const refreshHandler = () => {
+    getMembers();
+  };
 
   return (
     <AppLayout>
@@ -30,6 +38,11 @@ export function TeamsPage() {
       </p>
       <div>
         <div className="flex justify-between rounded-lg bg-[#F7F9FB] px-3 py-2">
+          <div className="mt-3 ml-2">
+            <button disabled={isFetchingMembers} onClick={refreshHandler}>
+              <RefreshIcon />
+            </button>
+          </div>
           <div className="flex items-center gap-3"></div>
           <div>
             <Button
