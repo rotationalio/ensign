0c01c6555f4c892e8175bef8ecb40eb522b8d1c7
Benjamin Bengfort <benjamin@rotational.io>
2023-07-12 17:18:51 -0500

    Quarterdeck Revoke Keys Command (#552)

--- cmd/quarterdeck/main.go
+++ cmd/quarterdeck/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/rotationalio/ensign/pkg"
 	"github.com/rotationalio/ensign/pkg/quarterdeck"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/config"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/db"
+	"github.com/rotationalio/ensign/pkg/quarterdeck/db/models"
 	"github.com/rotationalio/ensign/pkg/quarterdeck/report"
 	"github.com/rotationalio/ensign/pkg/utils/emails"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 	"github.com/urfave/cli/v2"
 )
+
+var conf config.Config
 
 func main() {
 	// If a dotenv file exists load it for configuration
@@ -40,7 +46,19 @@ func main() {
 			Usage:    "runs the daily PLG report",
 			Category: "utility",
 			Action:   runDailyUsersReport,
+			Before:   connectDB,
+			After:    closeDB,
 			Flags:    []cli.Flag{},
+		},
+		{
+			Name:      "revoke",
+			Usage:     "manually revoke an API key the hard way",
+			Category:  "utility",
+			Action:    revoke,
+			Before:    connectDB,
+			After:     closeDB,
+			ArgsUsage: "clientID [clientID ...]",
+			Flags:     []cli.Flag{},
 		},
 	}
 
@@ -71,16 +89,6 @@ func serve(c *cli.Context) (err error) {
 }
 
 func runDailyUsersReport(c *cli.Context) (err error) {
-	var conf config.Config
-	if conf, err = config.New(); err != nil {
-		return cli.Exit(err, 1)
-	}
-
-	// Ensure we're connected to the database
-	if err = db.Connect(conf.Database.URL, true); err != nil {
-		return cli.Exit(err, 1)
-	}
-
 	emailer := &Emailer{conf: conf}
 	if emailer.sendgrid, err = emails.New(conf.SendGrid); err != nil {
 		return cli.Exit(err, 1)
@@ -92,6 +100,60 @@ func runDailyUsersReport(c *cli.Context) (err error) {
 	}
 
 	if err = daily.Report(); err != nil {
+		return cli.Exit(err, 1)
+	}
+	return nil
+}
+
+func revoke(c *cli.Context) (err error) {
+	if c.NArg() < 1 {
+		return cli.Exit("specify at least one clientID of an API key to revoke", 1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	args := c.Args()
+	for i := 0; i < c.NArg(); i++ {
+		// Lookup the clientID in the database.
+		var apikey *models.APIKey
+		if apikey, err = models.GetAPIKey(ctx, args.Get(i)); err != nil {
+			if errors.Is(err, models.ErrNotFound) {
+				fmt.Printf("could not find key with client ID %q\n", args.Get(i))
+			} else {
+				return cli.Exit(fmt.Errorf("could not retrieve key with client ID %q: %w", args.Get(i), err), 1)
+			}
+		}
+
+		// Delete (revoke) the API key from the database.
+		if err = models.DeleteAPIKey(ctx, apikey.ID, apikey.OrgID); err != nil {
+			return cli.Exit(fmt.Errorf("could not revoke key with client ID %q: %w", apikey.KeyID, err), 1)
+		}
+		fmt.Printf("revoked key %q (%s) with clientID %s\n", apikey.Name, apikey.ID, apikey.KeyID)
+	}
+
+	return nil
+}
+
+//===========================================================================
+// Before and After Actions
+//===========================================================================
+
+func connectDB(c *cli.Context) (err error) {
+	if conf, err = config.New(); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	// Ensure we're connected to the database
+	if err = db.Connect(conf.Database.URL, true); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	return nil
+}
+
+func closeDB(c *cli.Context) (err error) {
+	if err = db.Close(); err != nil {
 		return cli.Exit(err, 1)
 	}
 	return nil
