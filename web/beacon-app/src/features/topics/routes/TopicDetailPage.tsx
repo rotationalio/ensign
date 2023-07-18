a1034a26368af458461e90251fce90ec24554bfa
daniellemaxwell <danielle@rotational.io>
2023-07-18 15:10:58 -0400

    Adds topic name and settings to detail page

--- web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
+++ web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
@@ -4,16 +4,25 @@ import { useParams } from 'react-router-dom';
 
 import AppLayout from '@/components/layout/AppLayout';
 
+import TopicSettings from '../components/TopicSettings';
+import { useFetchTopic } from '../hooks/useFetchTopic';
+
 const TopicDetailPage = () => {
-  const param = useParams<{ id: string }>();
+  const param = useParams();
   const { id: topicID } = param;
+  const { topic } = useFetchTopic(topicID as string);
 
   invariant(topicID, 'topic id is required');
   return (
     <AppLayout>
-      <Heading as="h1" className="flex items-center text-lg font-semibold">
-        Topic Name
-      </Heading>
+      <div className="flex items-center justify-between rounded-md bg-[#F7F9FB] px-6 py-3">
+        <Heading as="h1" className="flex items-center text-lg font-semibold">
+          <span className="mr-2" data-cy="topic-name">
+            {topic?.topic_name}
+          </span>
+        </Heading>
+        <TopicSettings />
+      </div>
       {/*      <Suspense
         fallback={
           <div>
