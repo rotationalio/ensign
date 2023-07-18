a46f46406810a23c66fd7f0f68e090bee3bf7299
Danielle <danielle@rotational.io>
2023-07-18 13:23:04 -0400

    Sc 18864 (#554)

--- web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
+++ web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
@@ -1,5 +1,28 @@
+import { Heading } from '@rotational/beacon-core';
+import invariant from 'invariant';
+import { useParams } from 'react-router-dom';
+
+import AppLayout from '@/components/layout/AppLayout';
+
 const TopicDetailPage = () => {
-  return <div>TopicDetailPage</div>;
+  const param = useParams<{ id: string }>();
+  const { id: topicID } = param;
+
+  invariant(topicID, 'topic id is required');
+  return (
+    <AppLayout>
+      <Heading as="h1" className="flex items-center text-lg font-semibold">
+        Topic Name
+      </Heading>
+      {/*      <Suspense
+        fallback={
+          <div>
+            <Loader />
+          </div>
+        }
+      ></Suspense> */}
+    </AppLayout>
+  );
 };
 
 export default TopicDetailPage;
