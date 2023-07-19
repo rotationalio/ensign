59fc38658e73c6eac574c052775ba6aaff738f35
Cletus Razakou <masskoder@gmail.com>
2023-07-19 18:01:37 -0400

    feat: adds topic stats api service

---
+++ web/beacon-app/src/features/topics/api/__tests__/topicStatsApiService.spec.ts
@@ -0,0 +1,58 @@
+import { vi } from 'vitest';
+
+import { APP_ROUTE } from '@/constants';
+
+import type { TopicQuickViewResponse } from '../../types/topicService';
+import topicStatsApiRequest from '../topicStatsApiService';
+vi.mock('invariant');
+// vi.mock('axios');
+
+describe('Topics Stats API Service ', () => {
+  describe('topicsStatsRequest', () => {
+    it('returns request resolved with response', async () => {
+      const mockDTO = {
+        topicID: '1',
+      } as any;
+
+      const mockResponse = {
+        data: [
+          {
+            name: 'publishers',
+            value: 2,
+          },
+          {
+            name: 'subscribers',
+            value: 3,
+          },
+          {
+            name: 'total_events',
+            value: 1000000,
+          },
+          {
+            name: 'storage',
+            value: 203,
+            units: 'MB',
+          },
+        ],
+      } as TopicQuickViewResponse;
+
+      const requestSpy = vi.fn().mockReturnValueOnce({
+        status: 200,
+        data: mockResponse.data,
+        statusText: 'OK',
+      });
+
+      const request = topicStatsApiRequest(requestSpy);
+      const response = await request(mockDTO.projectID);
+      expect(response).toBe(mockResponse);
+      expect(requestSpy).toHaveBeenCalledTimes(1);
+      // should return request payload
+      expect(requestSpy).toHaveBeenCalledWith(
+        `${APP_ROUTE.PROJECTS}/${mockDTO.projectID}${APP_ROUTE.TOPICS}`,
+        {
+          method: 'GET',
+        }
+      );
+    });
+  });
+});
