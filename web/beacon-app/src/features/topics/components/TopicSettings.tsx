d650e272489b0a7c1e945e3df0c610ee69e5fe6c
daniellemaxwell <danielle@rotational.io>
2023-07-19 12:27:53 -0400

    Adds archive topic modal

--- web/beacon-app/src/features/topics/components/TopicSettings.tsx
+++ web/beacon-app/src/features/topics/components/TopicSettings.tsx
@@ -1,32 +1,48 @@
 import { Trans } from '@lingui/macro';
 import { Button, Menu, useMenu } from '@rotational/beacon-core';
+import { useState } from 'react';
 
 import SettingIcon from '@/components/icons/setting';
 
+import ArchiveTopicModal from './ArchiveTopicModal';
+
 const TopicSettings = () => {
   const { isOpen, close, open, anchorEl } = useMenu({ id: 'topic-menu-action' });
+  const [isArchiveModalOpen, setIsArchiveModalOpen] = useState<boolean>(false);
+
+  const openArchiveModal = () => {
+    setIsArchiveModalOpen(true);
+  };
+
+  const onCloseArchiveModal = () => {
+    setIsArchiveModalOpen(false);
+  };
+
   return (
     <>
-      <Button
-        variant="ghost"
-        size="custom"
-        className="flex-end bg-inherit hover:bg-transparent border-none"
-        onClick={open}
-        data-cy="topicDetailActions"
-      >
-        <SettingIcon />
-      </Button>
-      <Menu open={isOpen} onClose={close} anchorEl={anchorEl}>
-        <Menu.Item>
-          <Trans>Archive Topic</Trans>
-        </Menu.Item>
-        <Menu.Item>
-          <Trans>Delete Topic</Trans>
-        </Menu.Item>
-        <Menu.Item>
-          <Trans>Clone Topic</Trans>
-        </Menu.Item>
-      </Menu>
+      <div>
+        <Button
+          variant="ghost"
+          size="custom"
+          className="flex-end bg-inherit hover:bg-transparent border-none"
+          onClick={open}
+          data-cy="topicDetailActions"
+        >
+          <SettingIcon />
+        </Button>
+        <Menu open={isOpen} onClose={close} anchorEl={anchorEl}>
+          <Menu.Item onClick={openArchiveModal}>
+            <Trans>Archive Topic</Trans>
+          </Menu.Item>
+          <Menu.Item>
+            <Trans>Delete Topic</Trans>
+          </Menu.Item>
+          <Menu.Item>
+            <Trans>Clone Topic</Trans>
+          </Menu.Item>
+        </Menu>
+      </div>
+      <ArchiveTopicModal isOpen={isArchiveModalOpen} close={onCloseArchiveModal} />
     </>
   );
 };
