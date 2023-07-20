a5cf105f59cd0b901a1c13b53bfbd6ffd728d1fd
daniellemaxwell <danielle@rotational.io>
2023-07-20 10:29:47 -0400

    Adds topic tooltip

--- web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
+++ web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
@@ -3,14 +3,35 @@ import invariant from 'invariant';
 import { useParams } from 'react-router-dom';
 
 import AppLayout from '@/components/layout/AppLayout';
+import DetailTooltip from '@/components/ui/Tooltip/DetailTooltip';
+import { formatDate } from '@/utils/formatDate';
 
 import TopicsBreadcrumbs from '../components/TopicsBreadcrumbs';
 import TopicSettings from '../components/TopicSettings';
 import { useFetchTopic } from '../hooks/useFetchTopic';
+
 const TopicDetailPage = () => {
   const param = useParams();
   const { id: topicID } = param;
   const { topic } = useFetchTopic(topicID as string);
+  const topicData = [
+    {
+      label: 'Topic ID',
+      value: topic?.id,
+    },
+    {
+      label: 'Status',
+      value: topic?.status,
+    },
+    {
+      label: 'Created',
+      value: formatDate(new Date(topic?.created as string)),
+    },
+    {
+      label: 'Modified',
+      value: formatDate(new Date(topic?.modified as string)),
+    },
+  ];
 
   invariant(topicID, 'topic id is required');
   return (
@@ -20,6 +41,7 @@ const TopicDetailPage = () => {
           <span className="mr-2" data-cy="topic-name">
             {topic?.topic_name}
           </span>
+          <DetailTooltip data={topicData} />
         </Heading>
         <TopicSettings />
       </div>
