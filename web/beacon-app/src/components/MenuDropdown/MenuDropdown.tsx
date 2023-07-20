5f505f2b9c6c08c806e0566e7f1aec27ae5acd56
daniellemaxwell <danielle@rotational.io>
2023-07-20 12:28:55 -0400

    Makes menu background darker in light mode

--- web/beacon-app/src/components/MenuDropdown/MenuDropdown.tsx
+++ web/beacon-app/src/components/MenuDropdown/MenuDropdown.tsx
@@ -28,7 +28,7 @@ const MenuDropdownMenu = ({ items, isOpen, onOpenChange }: RadixDropdownMenuProp
             className={clsx(
               'radix-side-bottom:animate-slide-down radix-side-top:animate-slide-up',
               'shadow-md w-48 rounded-lg px-1.5 py-1 md:w-56',
-              'bg-white dark:bg-gray-800'
+              'bg-gray-800'
             )}
           >
             {items?.generalMenuItems?.map(({ label, icon, shortcut, onClick }: any, i: any) => (
@@ -37,32 +37,30 @@ const MenuDropdownMenu = ({ items, isOpen, onOpenChange }: RadixDropdownMenuProp
                 onClick={onClick}
                 className={clsx(
                   'flex cursor-default select-none items-center rounded-md px-2 py-2 text-xs outline-none',
-                  'focus:bg-gray-50 text-gray-400 dark:text-gray-500 dark:focus:bg-gray-900'
+                  'text-gray-500 focus:bg-gray-900'
                 )}
               >
                 {icon}
-                <span className="flex-grow text-gray-700 dark:text-gray-300">{label}</span>
+                <span className="flex-grow text-gray-300">{label}</span>
                 {shortcut && <span className="text-xs">{shortcut}</span>}
               </DropdownMenuPrimitive.Item>
             ))}
 
             {items?.organizationMenuItems?.length > 0 && (
               <>
-                <DropdownMenuPrimitive.Separator className="my-1 h-px bg-gray-200 dark:bg-gray-700" />
-                <DropdownMenuPrimitive.Label className="select-none px-2 py-2 text-xs text-gray-700 dark:text-gray-200">
+                <DropdownMenuPrimitive.Separator className="my-1 h-px bg-gray-700" />
+                <DropdownMenuPrimitive.Label className="select-none px-2 py-2 text-xs text-gray-200">
                   Organization Management
                 </DropdownMenuPrimitive.Label>
                 <DropdownMenuPrimitive.Sub>
                   <DropdownMenuPrimitive.SubTrigger
                     className={clsx(
                       'flex w-full cursor-default select-none items-center rounded-md px-2 py-2 text-xs outline-none',
-                      'focus:bg-gray-50 text-gray-400 dark:text-gray-500 dark:focus:bg-gray-900'
+                      'text-gray-500 focus:bg-gray-900'
                     )}
                   >
                     <Link2Icon className="h-3.5 w-3.5 mr-2" />
-                    <span className="flex-grow text-gray-700 dark:text-gray-300">
-                      Switch Organization
-                    </span>
+                    <span className="flex-grow text-gray-300">Switch Organization</span>
                     <CaretRightIcon className="h-3.5 w-3.5" />
                   </DropdownMenuPrimitive.SubTrigger>
                   <DropdownMenuPrimitive.Portal>
@@ -70,7 +68,7 @@ const MenuDropdownMenu = ({ items, isOpen, onOpenChange }: RadixDropdownMenuProp
                       className={clsx(
                         'radix-side-right:animate-scale-in·origin-radix-dropdown-menu',
                         'shadow-md w-48 rounded-md px-1 py-1 text-xs',
-                        'bg-white dark:bg-gray-800'
+                        'bg-gray-800'
                       )}
                     >
                       {items?.organizationMenuItems?.map(
@@ -80,17 +78,17 @@ const MenuDropdownMenu = ({ items, isOpen, onOpenChange }: RadixDropdownMenuProp
                             key={`${name}-${i}`}
                             className={clsx(
                               'flex w-full cursor-default select-none items-center truncate rounded-md px-2 py-2 text-xs outline-none',
-                              'focus:bg-gray-50 text-gray-400 dark:text-gray-500 dark:focus:bg-gray-900'
+                              'text-gray-500 focus:bg-gray-900'
                             )}
                           >
-                            <span className="text-gray-700 dark:text-gray-300">{name}</span>
+                            <span className="text-gray-300">{name}</span>
                           </DropdownMenuPrimitive.Item>
                         )
                       )}
                     </DropdownMenuPrimitive.SubContent>
                   </DropdownMenuPrimitive.Portal>
                 </DropdownMenuPrimitive.Sub>
-                <DropdownMenuPrimitive.Separator className="my-1 h-px bg-gray-200 dark:bg-gray-700" />
+                <DropdownMenuPrimitive.Separator className="my-1 h-px bg-gray-700" />
               </>
             )}
             {items?.logoutMenuItem && (
@@ -98,13 +96,11 @@ const MenuDropdownMenu = ({ items, isOpen, onOpenChange }: RadixDropdownMenuProp
                 onClick={items?.logoutMenuItem?.onClick}
                 className={clsx(
                   'flex cursor-default select-none items-center rounded-md px-2 py-2 text-xs outline-none',
-                  'focus:bg-gray-50 text-gray-400 dark:text-gray-500 dark:focus:bg-gray-900'
+                  'text-gray-500 focus:bg-gray-900'
                 )}
               >
                 <Link2Icon className="h-3.5 w-3.5 mr-2" />
-                <span className="flex-grow text-gray-700 dark:text-gray-300">
-                  {items?.logoutMenuItem?.label}
-                </span>
+                <span className="flex-grow text-white">{items?.logoutMenuItem?.label}</span>
               </DropdownMenuPrimitive.Item>
             )}
           </DropdownMenuPrimitive.Content>
