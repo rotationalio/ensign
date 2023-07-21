bae3dc6dbb9ca53c238c2da47c8a7e2b57256e0e
Cletus Razakou <masskoder@gmail.com>
2023-07-21 18:11:49 -0400

    refact: update topic detail apge

--- web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
+++ web/beacon-app/src/features/topics/routes/TopicDetailPage.tsx
@@ -1,7 +1,8 @@
 import { Heading } from '@rotational/beacon-core';
-import invariant from 'invariant';
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { PATH_DASHBOARD } from '@/application';
 import AppLayout from '@/components/layout/AppLayout';
 import DetailTooltip from '@/components/ui/Tooltip/DetailTooltip';
 
@@ -13,11 +14,19 @@ import TopicSettings from '../components/TopicSettings';
 import { useFetchTopic } from '../hooks/useFetchTopic';
 import { getFormattedTopicData } from '../utils';
 const TopicDetailPage = () => {
+  const navigate = useNavigate();
   const param = useParams();
-  const { id: topicID } = param;
-  const { topic } = useFetchTopic(topicID as string);
+  const { id: topicID } = param as { id: string };
+  const { topic, error } = useFetchTopic(topicID);
 
-  invariant(topicID, 'topic id is required');
+  // if user switch to another organization and topic is not found then
+  // we need to redirect the user to the projects page
+  useEffect(() => {
+    if (error && error.response.status === 401) {
+      navigate(PATH_DASHBOARD.PROJECTS);
+    }
+  }, [error, navigate]);
+
   return (
     <AppLayout Breadcrumbs={<TopicsBreadcrumbs topic={topic} />}>
       <div className="flex items-center justify-between rounded-md bg-[#F7F9FB] px-6 py-3">
