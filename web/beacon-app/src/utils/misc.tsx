53cd22eac6adb4b5143e1f36a20c0a39e90b79aa
Cletus Razakou <masskoder@gmail.com>
2023-07-27 12:52:53 -0400

    add active effect on project

--- web/beacon-app/src/utils/misc.tsx
+++ web/beacon-app/src/utils/misc.tsx
@@ -1,1 +1,14 @@
 export const StatusIconMap = {};
+
+export const isCurrentMenuPath = (href: string, pathname: string) => {
+  const hrefArray = href.split('/');
+  const pathnameArray = pathname.split('/');
+
+  if (hrefArray.length > 2 && pathnameArray.length > 2) {
+    return hrefArray[1] === pathnameArray[1] && hrefArray[2] === pathnameArray[2];
+  }
+
+  if (href === pathname) {
+    return true;
+  }
+};
