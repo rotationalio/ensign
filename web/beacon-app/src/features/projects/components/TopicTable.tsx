4672bb3f00850b964d31483ef75e7752847e283d
Cletus Razakou <masskoder@gmail.com>
2023-07-31 17:05:11 -0400

    remove console log & minor update

--- web/beacon-app/src/features/projects/components/TopicTable.tsx
+++ web/beacon-app/src/features/projects/components/TopicTable.tsx
@@ -1,6 +1,7 @@
-import { Trans } from '@lingui/macro';
-import { Button, Loader, Table, Toast } from '@rotational/beacon-core';
-import { useMemo, useState } from 'react';
+import { t, Trans } from '@lingui/macro';
+import { Button, Loader, Table } from '@rotational/beacon-core';
+import { useEffect, useMemo, useState } from 'react';
+import toast from 'react-hot-toast';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { PATH_DASHBOARD } from '@/application';
@@ -23,58 +24,53 @@ export const TopicTable = () => {
 
   const { topics, isFetchingTopics, hasTopicsFailed, error } = useFetchTopics(projID);
 
-  console.log('topics data', topics); // do not remove this line. it is used for debugging since the topic id is not available in the UI
-
   const redirectToTopicDetails = (topicID: string) => {
+    if (!topicID) {
+      toast.error(t`Topic ID is missing`);
+    }
     navigate(`${PATH_DASHBOARD.TOPICS}/${topicID}`);
   };
 
-  if (isFetchingTopics) {
-    //
-    // TODO: add loading state
-    return <Loader />;
-  }
-
-  if (error) {
-    return (
-      <Toast
-        isOpen={hasTopicsFailed}
-        duration={3000}
-        variant="danger"
-        title="Sorry we are having trouble fetching your topics, please try again later."
-        description={(error as any)?.response?.data?.error}
-      />
-    );
-  }
+  useEffect(() => {
+    if (error && hasTopicsFailed) {
+      toast.error((error as any)?.response?.data?.error);
+    }
+  }, [error, hasTopicsFailed]);
 
   return (
     <div className="mt-[46px]" data-cy="topicComp">
-      <TopicTableHeader />
-      <div className="flex w-full justify-between bg-[#F7F9FB] p-2">
-        <div className="flex items-center gap-3"></div>
-        <Button
-          variant="primary"
-          size="small"
-          className="!text-xs"
-          onClick={handleOpenNewTopicModal}
-          data-cy="addTopic"
-        >
-          <Trans>+ New Topic</Trans>
-        </Button>
-        <NewTopicModal open={openNewTopicModal} handleClose={handleCloseNewTopicModal} />
-      </div>
-      <div className="overflow-hidden text-sm">
-        <Table
-          trClassName="text-sm"
-          columns={initialColumns}
-          data={getTopics(topics)}
-          onRowClick={(row: any) => {
-            redirectToTopicDetails(row?.values?.id);
-          }}
-          initialState={getHiddenColumns(['id'])}
-          data-cy="topicTable"
-        />
-      </div>
+      {isFetchingTopics ? (
+        <Loader />
+      ) : (
+        <>
+          <TopicTableHeader />
+          <div className="flex w-full justify-between bg-[#F7F9FB] p-2">
+            <div className="flex items-center gap-3"></div>
+            <Button
+              variant="primary"
+              size="small"
+              className="!text-xs"
+              onClick={handleOpenNewTopicModal}
+              data-cy="addTopic"
+            >
+              <Trans>+ New Topic</Trans>
+            </Button>
+            <NewTopicModal open={openNewTopicModal} handleClose={handleCloseNewTopicModal} />
+          </div>
+          <div className="overflow-hidden text-sm">
+            <Table
+              trClassName="text-sm"
+              columns={initialColumns}
+              data={getTopics(topics)}
+              onRowClick={(row: any) => {
+                redirectToTopicDetails(row?.values?.id);
+              }}
+              initialState={getHiddenColumns(['id'])}
+              data-cy="topicTable"
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
