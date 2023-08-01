2b9c4c531f4518117edcbbe597af714c43262112
Cletus Razakou <masskoder@gmail.com>
2023-08-01 13:41:19 -0400

    clean up

--- web/beacon-app/src/features/topics/utils.ts
+++ web/beacon-app/src/features/topics/utils.ts
@@ -59,8 +59,11 @@ export const getFormattedTopicData = (topic: Topic) => {
 export const inputSanitizer = (input: string) => {
   //  prevent XSS attacks
   const sanitizedInput = DOMPurify.sanitize(input);
+  // prevent SQL injection
   const sanitizedSqlInjection = sanitizedInput.replace(/'/g, "\\'");
+  // prevent JS injection
   const jsInjectionSafeInput = sanitizedSqlInjection.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  // prevent leading and trailing spaces
   const finalSanitizedInput = jsInjectionSafeInput.trim();
 
   return finalSanitizedInput;
