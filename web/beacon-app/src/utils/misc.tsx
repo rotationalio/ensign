2ed742dac976874c61a580995b8a06bd33261856
Cletus Razakou <masskoder@gmail.com>
2023-08-01 12:21:04 -0400

    refact: updates current path abstraction function

--- web/beacon-app/src/utils/misc.tsx
+++ web/beacon-app/src/utils/misc.tsx
@@ -6,8 +6,8 @@ export const isCurrentMenuPath = (href: string, pathname: string, href_linked?: 
   const hrefLinkedArray = href_linked?.split('/') || [];
   const pathnameArray = pathname.split('/');
 
-  if (href === PATH_DASHBOARD.PROJECTS && href_linked === PATH_DASHBOARD.TOPICS) {
-    return hrefArray[1] === pathnameArray[1] && hrefLinkedArray[1] === pathnameArray[1];
+  if (href_linked === PATH_DASHBOARD.TOPICS && pathnameArray[2] === 'topics') {
+    return hrefLinkedArray[1] === pathnameArray[1] && hrefLinkedArray[2] === pathnameArray[2];
   }
 
   if (hrefArray.length > 2 && pathnameArray.length > 2) {
