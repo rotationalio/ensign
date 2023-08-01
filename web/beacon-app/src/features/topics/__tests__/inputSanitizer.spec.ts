eed46fa47ecdd7935a587df27fdb2d2707fc0c5f
Cletus Razakou <masskoder@gmail.com>
2023-08-01 14:16:49 -0400

    test: js injection , xss attack , sql inject

---
+++ web/beacon-app/src/features/topics/__tests__/inputSanitizer.spec.ts
@@ -0,0 +1,44 @@
+import { inputSanitizer } from '../utils';
+
+describe('Input Sanitizer', () => {
+  it('should remove leading and trailing whitespaces from the input', () => {
+    const inputWithWhitespaces = '  Hello, World!   ';
+    const sanitizedInput = inputSanitizer(inputWithWhitespaces);
+
+    const expectedOutput = 'Hello, World!';
+
+    // Assertion
+    expect(sanitizedInput).toEqual(expectedOutput);
+  });
+
+  it('should prevent SQL injection', () => {
+    // Input data with potential SQL injection
+    const inputWithSqlInjection = "John'; DROP TABLE users;";
+    const sanitizedInput = inputSanitizer(inputWithSqlInjection);
+
+    const expectedOutput = "John\\'; DROP TABLE users;";
+
+    // Assertion
+    expect(sanitizedInput).toEqual(expectedOutput);
+  });
+
+  it('should prevent JS injection', () => {
+    // Input data with potential JS injection
+    const inputWithJsInjection = '<script>alert("XSS Attack");</script>';
+    const sanitizedInput = inputSanitizer(inputWithJsInjection);
+    const expectedOutput = '';
+
+    // Assertion
+    expect(sanitizedInput).toEqual(expectedOutput);
+  });
+
+  it('should prevent XSS attack', () => {
+    // Input data with potential XSS attack
+    const inputWithXssAttack = '<img src="invalid-url" onerror="alert(\'XSS Attack\');" />';
+    const sanitizedInput = inputSanitizer(inputWithXssAttack);
+    const expectedOutput = '&lt;img src="invalid-url"&gt;';
+
+    // Assertion
+    expect(sanitizedInput).toEqual(expectedOutput);
+  });
+});
