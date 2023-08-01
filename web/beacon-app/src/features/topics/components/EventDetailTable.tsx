a377c3065e5df7af718e9f69d2c21fb50a5a7ce3
Cletus Razakou <masskoder@gmail.com>
2023-08-01 17:39:45 -0400

    add input sanitizer  (#599)
    Co-authored-by: Benjamin Bengfort <benjamin@bengfort.com>
    Co-authored-by: Danielle <danielle@rotational.io>
    Co-authored-by: Patrick Deziel <deziel.patrick@gmail.com>

---
+++ web/beacon-app/src/features/topics/components/EventDetailTable.tsx
@@ -0,0 +1,27 @@
+import { Table } from '@rotational/beacon-core';
+import { ErrorBoundary } from '@sentry/react';
+import { useMemo } from 'react';
+
+import { getEventDetailColumns } from '../utils';
+
+const EventDetailTable = () => {
+  const initialColumns = useMemo(() => getEventDetailColumns(), []) as any;
+  return (
+    <div className="mx-4">
+      <ErrorBoundary
+        fallback={
+          <div className="item-center my-auto flex w-full text-center font-bold text-danger-500">
+            <p>
+              Sorry we are having trouble listing event details for your topic, please refresh the
+              page and try again.
+            </p>
+          </div>
+        }
+      >
+        <Table columns={initialColumns} data={[]} />
+      </ErrorBoundary>
+    </div>
+  );
+};
+
+export default EventDetailTable;
