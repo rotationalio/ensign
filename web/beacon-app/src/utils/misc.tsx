e254e9e5660e40f0211117d83347d7b2cdbf04ae
Cletus Razakou <masskoder@gmail.com>
2023-08-01 12:55:46 -0400

    Add active effect on project menu for topic detail view (#596)

--- web/beacon-app/src/utils/misc.tsx
+++ web/beacon-app/src/utils/misc.tsx
@@ -1,8 +1,14 @@
+import { PATH_DASHBOARD } from '@/application';
 export const StatusIconMap = {};
 
-export const isCurrentMenuPath = (href: string, pathname: string) => {
+export const isCurrentMenuPath = (href: string, pathname: string, href_linked?: string) => {
   const hrefArray = href.split('/');
+  const hrefLinkedArray = href_linked?.split('/') || [];
   const pathnameArray = pathname.split('/');
+
+  if (href_linked === PATH_DASHBOARD.TOPICS && pathnameArray[2] === 'topics') {
+    return hrefLinkedArray[1] === pathnameArray[1] && hrefLinkedArray[2] === pathnameArray[2];
+  }
 
   if (hrefArray.length > 2 && pathnameArray.length > 2) {
     return hrefArray[1] === pathnameArray[1] && hrefArray[2] === pathnameArray[2];
