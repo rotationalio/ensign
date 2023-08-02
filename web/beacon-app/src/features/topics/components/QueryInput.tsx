618e2887befe74e217135aeff1bdd6f4cb0396fd
daniellemaxwell <danielle@rotational.io>
2023-08-02 18:32:09 -0400

    Clears topic query input

--- web/beacon-app/src/features/topics/components/QueryInput.tsx
+++ web/beacon-app/src/features/topics/components/QueryInput.tsx
@@ -1,24 +1,34 @@
 import { Trans } from '@lingui/macro';
 import { Button, TextField } from '@rotational/beacon-core';
+import { useEffect, useState } from 'react';
 
 type TopicNameProps = {
   name: string;
 };
 
 const QueryInput = ({ name }: TopicNameProps) => {
+  const [topicQuery, setTopicQuery] = useState('');
+
+  useEffect(() => {
+    setTopicQuery(`SELECT * FROM ${name} LIMIT 10`);
+  }, [name]);
+
+  const handleInputQueryChange = (e: any) => {
+    setTopicQuery(e.target.value);
+  };
+
+  const handleClearInputQuery = () => {
+    setTopicQuery('');
+  };
+
   return (
     <div className="mt-4 flex space-x-2">
-      <TextField
-        labelClassName="Topic Query"
-        type="search"
-        value={`SELECT * FROM ${name} LIMIT 1`}
-        fullWidth
-      ></TextField>
+      <TextField type="search" value={topicQuery} fullWidth onChange={handleInputQueryChange} />
       <div className="flex space-x-2">
         <Button variant="secondary">
           <Trans>Query</Trans>
         </Button>
-        <Button>
+        <Button onClick={handleClearInputQuery}>
           <Trans>Clear</Trans>
         </Button>
       </div>
