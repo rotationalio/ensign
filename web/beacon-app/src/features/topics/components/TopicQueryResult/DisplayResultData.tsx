c82260a54ecbd1e256758ca2a6b5260c2084149f
Cletus Razakou <masskoder@gmail.com>
2023-08-03 15:48:52 -0400

    feat: add binary result component

--- web/beacon-app/src/features/topics/components/TopicQueryResult/DisplayResultData.tsx
+++ web/beacon-app/src/features/topics/components/TopicQueryResult/DisplayResultData.tsx
@@ -10,8 +10,38 @@ interface DisplayResultDataProps {
   isBase64Encoded?: boolean;
 }
 
+type BinaryResultProps = {
+  data: ArrayBuffer | null;
+};
+
+const BinaryResult: React.FC<BinaryResultProps> = ({ data }) => {
+  const formatBinaryData = (binaryData: ArrayBuffer | null): string => {
+    if (!binaryData) return '';
+
+    // Convert the binary data to a Uint8Array
+    const uint8Array = new Uint8Array(binaryData);
+    // Convert the Uint8Array to a hexadecimal string
+    const hexString = uint8Array.reduce(
+      (acc, byte) => acc + byte.toString(16).padStart(2, '0'),
+      ''
+    );
+    // Add spaces between every two hexadecimal characters for better readability
+    const formattedHexString = hexString.replace(/(..)/g, '$1 ');
+
+    return formattedHexString;
+  };
+
+  const formattedData = formatBinaryData(data);
+
+  return <>{formattedData}</>;
+};
+
 // Default component that should be rendered if the mimetype is binary based
 const renderDefaultResultComponent = (result: any) => {
+  if (result instanceof ArrayBuffer) {
+    return <BinaryResult data={result} />;
+  }
+
   return <>{result}</>;
 };
 
