5fbb202b2de61f7c9f70d95758baa60a18bbbd4b
daniellemaxwell <danielle@rotational.io>
2023-08-04 08:27:43 -0400

    Finalizes form functionality

--- web/beacon-app/src/features/topics/components/QueryForm.tsx
+++ web/beacon-app/src/features/topics/components/QueryForm.tsx
@@ -1,36 +1,44 @@
 import { Trans } from '@lingui/macro';
 import { Button, TextField } from '@rotational/beacon-core';
+import { Form, FormikHelpers, FormikProvider } from 'formik';
+
+import { ProjectQueryDTO } from '@/features/projects/types/projectQueryService';
+
+import { useTopicQueryInputForm } from '../schemas/topicQueryInputValidationSchema';
 
 type QueryFormProps = {
   defaultEnSQL: string;
-  queryHandler: (query: string) => void;
+  isSubmitting?: boolean;
+  onSubmit: (values: ProjectQueryDTO, formikHelpers: FormikHelpers<ProjectQueryDTO>) => void;
 };
 
-const QueryForm = ({ defaultEnSQL, queryHandler }: QueryFormProps) => {
-  const onQuery = (e: any) => {
-    e.preventDefault();
-    queryHandler(e.target.value);
-  };
+const QueryForm = ({ defaultEnSQL, isSubmitting, onSubmit }: QueryFormProps) => {
+  const formik = useTopicQueryInputForm(onSubmit);
+
+  const { handleReset, handleSubmit, getFieldProps, touched, errors } = formik;
 
   return (
-    <div className="mt-4 flex space-x-2">
-      <TextField
-        labelClassName="font-semibold"
-        type="search"
-        defaultValue={defaultEnSQL}
-        onChange={onQuery}
-        name="query"
-        fullWidth
-      ></TextField>
-      <div className="flex space-x-2">
-        <Button variant="secondary" onClick={onQuery}>
-          <Trans>Query</Trans>
-        </Button>
-        <Button>
-          <Trans>Clear</Trans>
-        </Button>
-      </div>
-    </div>
+    <FormikProvider value={formik}>
+      <Form>
+        <div className="mt-4 flex space-x-2">
+          <TextField
+            placeholder={defaultEnSQL}
+            fullWidth
+            errorMessage={touched.query && errors.query}
+            {...getFieldProps('query')}
+            name="query"
+          />
+          <div className="flex max-h-[44px] space-x-2">
+            <Button variant="secondary" onclick={handleSubmit} isLoading={isSubmitting}>
+              <Trans>Query</Trans>
+            </Button>
+            <Button onClick={handleReset}>
+              <Trans>Clear</Trans>
+            </Button>
+          </div>
+        </div>
+      </Form>
+    </FormikProvider>
   );
 };
 
--- web/beacon-app/src/features/topics/schemas/topicQueryInputValidationSchema.ts
+++ web/beacon-app/src/features/topics/schemas/topicQueryInputValidationSchema.ts
@@ -2,17 +2,23 @@ import { t } from '@lingui/macro';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
+import { ProjectQueryDTO } from '@/features/projects/types/projectQueryService';
+
 export const topicQuerySchema = Yup.object().shape({
   query: Yup.string()
     .trim()
     .required(t`Topic query is required.`),
 });
 
-export const QUERY_INPUT_FORM_OPTIONS = (onSubmit: any, initialValues?: any) => ({
-  initialValues: initialValues || '',
+const TOPIC_QUERY_INITIAL_VALUES = {
+  query: '',
+} satisfies Omit<ProjectQueryDTO, 'projectID'>;
+
+export const QUERY_INPUT_FORM_OPTIONS = (onSubmit: any) => ({
+  initialValues: TOPIC_QUERY_INITIAL_VALUES,
   validationSchema: topicQuerySchema,
   onSubmit,
 });
 
-export const useTopicQueryInputForm = (onSubmit: any, initialValues?: any) =>
-  useFormik(QUERY_INPUT_FORM_OPTIONS(onSubmit, initialValues));
+export const useTopicQueryInputForm = (onSubmit: any) =>
+  useFormik(QUERY_INPUT_FORM_OPTIONS(onSubmit));
