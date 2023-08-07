1fce13088cf2b8ef2a171cebcf9e5b8636bdf0e7
Cletus Razakou <masskoder@gmail.com>
2023-08-07 10:11:02 -0400

    feat: add Tag component

---
+++ web/beacon-app/src/components/ui/Tag/Tag.tsx
@@ -0,0 +1,70 @@
+import { mergeClassnames } from '@rotational/beacon-core';
+import React from 'react';
+export interface TagProps {
+  children: React.ReactNode;
+  variant?: 'primary' | 'success' | 'warning' | 'error' | 'ghost' | 'secondary';
+  className?: string;
+  size?: 'small' | 'medium' | 'large';
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+}
+
+const Tag: React.FC<TagProps> = ({
+  children,
+  variant,
+  className,
+  size = 'small',
+  leftIcon,
+  rightIcon,
+}) => {
+  const getVariantClasses = () => {
+    switch (variant) {
+      case 'primary':
+        return 'bg-primary text-white';
+      case 'secondary':
+        return 'bg-secondary text-white';
+      case 'success':
+        return 'bg-green-success text-white';
+      case 'warning':
+        return 'bg-warning-500 text-black';
+      case 'error':
+        return 'bg-danger-500 text-white';
+      case 'ghost':
+        return 'bg-transparent text-black';
+
+      default:
+        return 'bg-gray-400 text-black';
+    }
+  };
+
+  const getSizeClasses = () => {
+    switch (size) {
+      case 'small':
+        return 'text-xs py-1 px-2';
+      case 'medium':
+        return 'text-sm py-1 px-2';
+      case 'large':
+        return 'text-lg py-2 px-4';
+      default:
+        return 'text-sm py-1 px-2';
+    }
+  };
+
+  const combinedClassName = mergeClassnames(
+    'rounded-full py-1 px-2 inline-block',
+    getSizeClasses(),
+    getVariantClasses(),
+    className
+  );
+  return (
+    <>
+      <span className={combinedClassName}>
+        {leftIcon && <span className="mr-1">{leftIcon}</span>}
+        {children}
+        {rightIcon && <span className="ml-1">{rightIcon}</span>}
+      </span>
+    </>
+  );
+};
+
+export default Tag;
