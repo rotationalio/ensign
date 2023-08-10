a458f5a3844ce7784b786a7597890d355c1d9af4
Cletus Razakou <masskoder@gmail.com>
2023-08-10 15:11:49 -0400

    refactor/style: update heading size

--- web/beacon-app/src/features/auth/components/Register/SuccessfullAccountCreation.tsx
+++ web/beacon-app/src/features/auth/components/Register/SuccessfullAccountCreation.tsx
@@ -9,19 +9,19 @@ function SuccessfullAccountCreation() {
       <OtterLookingDown className="absolute -right-16 -top-[10.8rem]" />
       <Card contentClassName="border border-[#72A2C0] rounded-md p-4 md:p-8 text-sm">
         <Card.Header>
-          <h1 className="text-md font-bold">
+          <h1 className="text-[18px] font-bold">
             <Trans>Thank you for creating your Ensign account!</Trans>
           </h1>
         </Card.Header>
         <Card.Body>
-          <Heading as="h1" className="mt-4 mb-3">
+          <Heading as="h1" className="mt-4 mb-3 ">
             <Trans>
               We value security and care deeply about protecting your information. For that reason,
               we request that you verify your email account.{' '}
             </Trans>
           </Heading>
           <div className="space-y-2">
-            <Heading as="h2" className="text-sm font-semibold">
+            <Heading as="h2" className="text-md font-bold">
               <Trans>Next Steps</Trans>
             </Heading>
             <p>
