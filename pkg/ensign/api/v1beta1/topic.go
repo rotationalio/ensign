1a12750190b886f4b940042c959047681b0f47c4
Benjamin Bengfort <benjamin@bengfort.com>
2023-08-16 07:38:03 -0500

    Topic Info Gathering Process

--- pkg/ensign/api/v1beta1/topic.go
+++ pkg/ensign/api/v1beta1/topic.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 
 	"github.com/oklog/ulid/v2"
+	"github.com/rotationalio/ensign/pkg/ensign/rlid"
 	"github.com/twmb/murmur3"
 )
 
@@ -14,8 +15,8 @@ const (
 
 var topicNameRegex = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9\.\-\_]*$`)
 
-// ULID returns the ULID representation of the topic ID.
-func (t *Topic) ULID() (uid ulid.ULID, err error) {
+// ParseTopicID returns the ULID representation of the topic ID.
+func (t *Topic) ParseTopicID() (uid ulid.ULID, err error) {
 	uid = ulid.ULID{}
 	if err = uid.UnmarshalBinary(t.Id); err != nil {
 		return uid, err
@@ -52,4 +53,44 @@ func ValidTopicName(name string) bool {
 	}
 
 	return true
+}
+
+func (i *TopicInfo) ParseTopicID() (topicID ulid.ULID, err error) {
+	topicID = ulid.ULID{}
+	if err = topicID.UnmarshalBinary(i.TopicId); err != nil {
+		return topicID, err
+	}
+	return topicID, nil
+}
+
+func (i *TopicInfo) ParseProjectID() (projectID ulid.ULID, err error) {
+	projectID = ulid.ULID{}
+	if err = projectID.UnmarshalBinary(i.ProjectId); err != nil {
+		return projectID, err
+	}
+	return projectID, nil
+}
+
+func (i *TopicInfo) ParseEventOffsetID() (eventID rlid.RLID, err error) {
+	eventID = rlid.RLID{}
+	if err = eventID.UnmarshalBinary(i.EventOffsetId); err != nil {
+		return eventID, err
+	}
+	return eventID, nil
+}
+
+// Finds the event type info for the specified type in the type list. If it does not
+// exist, the event type info is created an appended to the type list.
+func (i *TopicInfo) FindEventTypeInfo(etype *Type) *EventTypeInfo {
+	// Look for existing event type info for the specified type
+	for _, einfo := range i.Types {
+		if einfo.Type.Equals(etype) {
+			return einfo
+		}
+	}
+
+	// Create event type info for the specified type
+	einfo := &EventTypeInfo{Type: etype}
+	i.Types = append(i.Types, einfo)
+	return einfo
 }
--- pkg/ensign/api/v1beta1/topic_test.go
+++ pkg/ensign/api/v1beta1/topic_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/oklog/ulid/v2"
 	api "github.com/rotationalio/ensign/pkg/ensign/api/v1beta1"
+	"github.com/rotationalio/ensign/pkg/ensign/rlid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -35,7 +36,7 @@ func TestTopicULID(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		uid, err := tc.topic.ULID()
+		uid, err := tc.topic.ParseTopicID()
 		if tc.err != nil {
 			require.EqualError(t, err, tc.err.Error())
 		} else {
@@ -94,4 +95,106 @@ func TestValidateTopicName(t *testing.T) {
 	for _, name := range invalid {
 		require.False(t, api.ValidTopicName(name), "expected %q to be invalid", name)
 	}
+}
+
+func TestTopicInfoParseTopicID(t *testing.T) {
+	testCases := []struct {
+		topicID  []byte
+		expected ulid.ULID
+		err      error
+	}{
+		{nil, ulid.ULID{}, ulid.ErrDataSize},
+		{[]byte{}, ulid.ULID{}, ulid.ErrDataSize},
+		{[]byte("foo"), ulid.ULID{}, ulid.ErrDataSize},
+		{ulid.MustParse("01H7Z2XD3VDEV0VKG8PF699ZGQ").Bytes(), ulid.MustParse("01H7Z2XD3VDEV0VKG8PF699ZGQ"), nil},
+	}
+
+	for i, tc := range testCases {
+		info := &api.TopicInfo{TopicId: tc.topicID}
+		topicID, err := info.ParseTopicID()
+		if tc.err != nil {
+			require.Error(t, err, "test case %d failed", i)
+			require.Equal(t, ulid.ULID{}, topicID, "test case %d failed", i)
+		} else {
+			require.NoError(t, err, "test case %d failed", i)
+			require.Equal(t, tc.expected, topicID, "test case %d failed", i)
+		}
+	}
+}
+
+func TestTopicInfoParseProjectID(t *testing.T) {
+	testCases := []struct {
+		projectID []byte
+		expected  ulid.ULID
+		err       error
+	}{
+		{nil, ulid.ULID{}, ulid.ErrDataSize},
+		{[]byte{}, ulid.ULID{}, ulid.ErrDataSize},
+		{[]byte("foo"), ulid.ULID{}, ulid.ErrDataSize},
+		{ulid.MustParse("01H7Z2XD3VDEV0VKG8PF699ZGQ").Bytes(), ulid.MustParse("01H7Z2XD3VDEV0VKG8PF699ZGQ"), nil},
+	}
+
+	for i, tc := range testCases {
+		info := &api.TopicInfo{ProjectId: tc.projectID}
+		projectID, err := info.ParseProjectID()
+		if tc.err != nil {
+			require.Error(t, err, "test case %d failed", i)
+			require.Equal(t, ulid.ULID{}, projectID, "test case %d failed", i)
+		} else {
+			require.NoError(t, err, "test case %d failed", i)
+			require.Equal(t, tc.expected, projectID, "test case %d failed", i)
+		}
+	}
+}
+
+func TestTopicInfoParseEventOffsetID(t *testing.T) {
+	testCases := []struct {
+		eventID  []byte
+		expected rlid.RLID
+		err      error
+	}{
+		{nil, rlid.RLID{}, rlid.ErrDataSize},
+		{[]byte{}, rlid.RLID{}, rlid.ErrDataSize},
+		{[]byte("foo"), rlid.RLID{}, rlid.ErrDataSize},
+		{rlid.MustParse("064zwbj8vg00000n").Bytes(), rlid.MustParse("064zwbj8vg00000n"), nil},
+	}
+
+	for i, tc := range testCases {
+		info := &api.TopicInfo{EventOffsetId: tc.eventID}
+		eventID, err := info.ParseEventOffsetID()
+		if tc.err != nil {
+			require.Error(t, err, "test case %d failed", i)
+			require.Equal(t, rlid.RLID{}, eventID, "test case %d failed", i)
+		} else {
+			require.NoError(t, err, "test case %d failed", i)
+			require.Equal(t, tc.expected, eventID, "test case %d failed", i)
+		}
+	}
+}
+
+func TestFindEventTypeInfo(t *testing.T) {
+	info := &api.TopicInfo{}
+	require.Len(t, info.Types, 0)
+
+	etype := info.FindEventTypeInfo(api.UnspecifiedType)
+	require.Len(t, info.Types, 1)
+	etype.Events = 42
+	etype.Duplicates = 1
+	etype.DataSizeBytes = 4096
+
+	compat := info.FindEventTypeInfo(api.UnspecifiedType)
+	require.Len(t, info.Types, 1)
+	require.Equal(t, uint64(42), compat.Events)
+	require.Equal(t, uint64(1), compat.Duplicates)
+	require.Equal(t, uint64(4096), compat.DataSizeBytes)
+
+	// Manually add a type info to the info
+	info.Types = append(info.Types, &api.EventTypeInfo{Type: &api.Type{Name: "TestType", MajorVersion: 1}, Events: 100, DataSizeBytes: 1.24e6})
+	require.Len(t, info.Types, 2)
+
+	etype = info.FindEventTypeInfo(&api.Type{Name: "TestType", MajorVersion: 1})
+	require.Len(t, info.Types, 2)
+	require.Equal(t, uint64(100), etype.Events)
+	require.Equal(t, uint64(0), etype.Duplicates)
+	require.Equal(t, uint64(1.24e6), etype.DataSizeBytes)
 }
