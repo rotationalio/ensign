88b2a72cab0a88c8dc9799455f9d1d91b4fb1bd2
Benjamin Bengfort <benjamin@bengfort.com>
2023-08-16 18:17:53 -0500

    Update Topic Tapble Header Copy and Data Flow Link

--- web/beacon-app/src/application/routes/paths.ts
+++ web/beacon-app/src/application/routes/paths.ts
@@ -50,4 +50,5 @@ export const EXTERNAL_LINKS = {
   TWITTER: 'https://twitter.com/rotationalio',
   ENSQL: 'https://ensign.rotational.dev/ensql/',
   OFFICE_HOURS_SCHEDULE: 'https://calendar.app.google/1r7PuDPzKp2jjHPX8',
+  DATA_FLOW_OVERVIEW: 'https://www.youtube.com/watch?v=XUnEHGZXxmM&t=1600s',
 };
