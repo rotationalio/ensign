0bd95b21ceaaa3c80a59f7257e9e445c4b224916
daniellemaxwell <danielle@rotational.io>
2023-08-30 19:46:49 -0400

    Adds views for steps 1 and 3 of onboarding workflow

--- web/beacon-app/src/features/onboarding/components/steps/name/index.tsx
+++ web/beacon-app/src/features/onboarding/components/steps/name/index.tsx
@@ -1,8 +1,33 @@
+import { Trans } from '@lingui/macro';
+import { Button, Heading, TextField } from '@rotational/beacon-core';
+
 const nameStep = () => {
   return (
-    <div>
-      <h1>nameStep</h1>
-    </div>
+    <>
+      <Heading as="h1" className="text-lg font-bold">
+        Step 3 of 4
+      </Heading>
+      <p className="mt-4 font-bold">
+        <Trans>What's your name?</Trans>
+      </p>
+      <p className="my-4">
+        <Trans>
+          Adding your name will make it easier for your teammates to collaborate with you.
+        </Trans>
+      </p>
+      <div className="max-w-6xl">
+        <TextField
+          fullWidth
+          placeholder="Ex. Haley Smith"
+          label="Name"
+          labelClassName="sr-only"
+          className="mb-4"
+        />
+      </div>
+      <Button variant="secondary">
+        <Trans>Next</Trans>
+      </Button>
+    </>
   );
 };
 
--- web/beacon-app/src/features/onboarding/components/steps/organization/index.tsx
+++ web/beacon-app/src/features/onboarding/components/steps/organization/index.tsx
@@ -1,8 +1,34 @@
+import { Trans } from '@lingui/macro';
+import { Button, Heading, TextField } from '@rotational/beacon-core';
+
 const OrganizationStep = () => {
   return (
-    <div>
-      <h1>OrganizationStep</h1>
-    </div>
+    <>
+      <Heading as="h1" className="text-lg font-bold">
+        Step 1 of 4
+      </Heading>
+      <p className="mt-4 font-bold">
+        <Trans>What's the name of your team or organization?</Trans>
+      </p>
+      <p className="my-4">
+        <Trans>
+          This will be the name of your workspace where you create projects and collaborate, so
+          choose something you and your teammates will recognize.
+        </Trans>
+      </p>
+      <div className="max-w-6xl">
+        <TextField
+          fullWidth
+          placeholder="Ex. Rotational Labs"
+          label="Team or Organization Name"
+          labelClassName="sr-only"
+          className="mb-4"
+        />
+      </div>
+      <Button variant="secondary">
+        <Trans>Next</Trans>
+      </Button>
+    </>
   );
 };
 
