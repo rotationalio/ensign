7fdc3af7943cb84273f1c21792201f185784615f
Danielle <danielle@rotational.io>
2023-08-30 12:52:32 -0400

    Updates verify account page copy (#671)

--- web/beacon-app/src/features/auth/components/Register/SuccessfullAccountCreation.tsx
+++ web/beacon-app/src/features/auth/components/Register/SuccessfullAccountCreation.tsx
@@ -1,37 +1,48 @@
 import { Trans } from '@lingui/macro';
-import { Card, Heading } from '@rotational/beacon-core';
+import { Button, Card, Heading } from '@rotational/beacon-core';
 
+import { ROUTES } from '@/application';
 import OtterLookingDown from '@/components/icons/otter-looking-down';
 
 function SuccessfullAccountCreation() {
+  const registrationEmail = sessionStorage.getItem('newRegistrationEmail');
+
   return (
     <div className="relative mx-auto mt-20 w-fit pt-20">
       <OtterLookingDown className="absolute -right-16 -top-[10.8rem]" />
       <Card contentClassName="border border-[#72A2C0] rounded-md p-4 md:p-8 text-sm">
         <Card.Header>
           <h1 className="text-[18px] font-bold">
-            <Trans>Thank you for creating your Ensign account!</Trans>
+            <Trans>Let's Go!</Trans>
           </h1>
         </Card.Header>
         <Card.Body>
           <Heading as="h1" className="mt-4 mb-3 ">
             <Trans>
-              We value security and care deeply about protecting your information. For that reason,
-              we request that you verify your email account.{' '}
+              To keep your account safe, we sent a verification email to{' '}
+              {registrationEmail ? (
+                <span className="font-bold">{registrationEmail}</span>
+              ) : (
+                'the email address provided during sign up'
+              )}
+              . Click the secure link in that email to continue.
             </Trans>
           </Heading>
-          <div className="space-y-2">
-            <Heading as="h2" className="text-md font-bold">
-              <Trans>Next Steps</Trans>
-            </Heading>
-            <p>
-              {' '}
-              <Trans>
-                Please check your email and click the secure link in the verification email we just
-                sent you. You can then log into Ensign to start building!
-              </Trans>
-            </p>
-          </div>
+          <p>
+            {' '}
+            <Trans>
+              Didn't receive an email?{' '}
+              <span className="font-bold text-[#1F4CED]">Resend the verification email</span> or
+              email{' '}
+              <a href={`mailto:${ROUTES.SUPPORT}`} className="font-bold text-[#1F4CED]">
+                support@rotational.io
+              </a>
+              .
+            </Trans>
+          </p>
+          <Button variant="primary" className="mt-4 font-bold text-white">
+            Resend verification email
+          </Button>{' '}
         </Card.Body>
       </Card>
     </div>
