1c2e796e434fa1b22f9465f4570df833f2d3579d
Cletus Razakou <masskoder@gmail.com>
2023-08-30 13:22:06 -0400

    fix: infinte loading after session expired

--- web/beacon-app/src/features/members/loaders/userLoader.tsx
+++ web/beacon-app/src/features/members/loaders/userLoader.tsx
@@ -2,30 +2,32 @@ import { json } from 'react-router-dom';
 
 import { queryClient } from '@/application/config/react-query';
 import { useOrgStore } from '@/store';
+import { clearCookies } from '@/utils/cookies';
 
 import { memberDetailQuery } from '../hooks/useFetchMember';
 
 const userLoader = () => async () => {
   const orgDataState = useOrgStore.getState() as any;
   const { user } = orgDataState || null;
+  console.log('userLoader', user);
 
   if (!user) {
-    return json({
-      member: null,
-    });
-  }
+    return null;
+  } else {
+    try {
+      const query = memberDetailQuery(user);
 
-  try {
-    const query = memberDetailQuery(user);
-
-    const member =
-      queryClient.getQueryData(query?.queryKey) ?? (await queryClient.fetchQuery(query));
-    return json({
-      member,
-    });
-  } catch (error: any) {
-    if (error?.response?.status === 401) {
-      window.location.href = '/';
+      const member =
+        queryClient.getQueryData(query?.queryKey) ?? (await queryClient.fetchQuery(query));
+      return json({
+        member,
+      });
+    } catch (error: any) {
+      if (error?.response?.status === 401) {
+        clearCookies();
+        // routeLoader.clearCacheAll(); // TODO: clear cache all routes when logout
+        window.location.href = '/';
+      }
     }
   }
 };
