e460699175de19d611082bf738cc8de9b31f0680
Cletus Razakou <masskoder@gmail.com>
2023-09-01 14:31:29 -0400

    Refactor: Enhance Maintainability of the Profession Segment

--- web/beacon-app/src/features/onboarding/components/steps/preference/profession/ProfessionSegment.tsx
+++ web/beacon-app/src/features/onboarding/components/steps/preference/profession/ProfessionSegment.tsx
@@ -1,68 +1,35 @@
 import CheckCircleIcon from '@/components/icons/check-circle';
 
+import { getProfessionOptions } from '../../../../utils';
 import Header from './Header';
 const ProfessionSegment = () => {
+  const PROFESSION_OPTIONS = getProfessionOptions();
   return (
     <div>
       <Header />
       <div className="my-5">
         <ul className="grid w-full gap-20 md:grid-cols-3">
-          <li>
-            <input
-              id="work_segment"
-              type="radio"
-              value={'work'}
-              name="profession_segment"
-              className="peer hidden"
-              required
-            />
-            <label
-              htmlFor="work_segment"
-              className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600"
-            >
-              <div className="block">
-                <div className="w-full text-lg font-bold">Work</div>
-              </div>
-              <CheckCircleIcon />
-            </label>
-          </li>
-          <li>
-            <input
-              id="education_segment"
-              type="radio"
-              value={'education'}
-              name="profession_segment"
-              className="peer hidden"
-            />
-            <label
-              htmlFor="education_segment"
-              className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600"
-            >
-              <div className="block">
-                <div className="w-full text-lg font-bold">Education</div>
-              </div>
-
-              <CheckCircleIcon />
-            </label>
-          </li>
-          <li>
-            <input
-              id="personal_segment"
-              type="radio"
-              name="profession_segment"
-              className="peer hidden"
-              value={'personal'}
-            />
-            <label
-              htmlFor="personal_segment"
-              className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600"
-            >
-              <div className="block">
-                <div className="w-full text-lg font-bold">Personal</div>
-              </div>
-              <CheckCircleIcon />
-            </label>
-          </li>
+          {PROFESSION_OPTIONS?.map((option: any, idx: any) => (
+            <li key={idx}>
+              <input
+                id={option.id}
+                type="radio"
+                value={option.value}
+                name="profession_segment"
+                className="peer hidden"
+                required
+              />
+              <label
+                htmlFor={option.id}
+                className="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border border-gray-200 bg-white p-5 text-gray-500 hover:bg-gray-100 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-blue-600"
+              >
+                <div className="block">
+                  <div className="w-full text-lg font-bold">{option.label}</div>
+                </div>
+                <CheckCircleIcon />
+              </label>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- web/beacon-app/src/features/onboarding/utils.ts
+++ web/beacon-app/src/features/onboarding/utils.ts
@@ -1,13 +1,30 @@
+import { t } from '@lingui/macro';
 export const getWorksOptions = () => {
   return [
-    { value: 'Application Development', label: 'Application Development' },
-    { value: 'Data science', label: 'Data science' },
-    { value: 'Developer experience', label: 'Developer experience' },
-    { value: 'Cybersecurity', label: 'Cybersecurity (blue or purple team)' },
-    { value: 'DevOps and observability', label: 'DevOps and observability' },
+    { value: 'Application Development', label: t`Application Development` },
+    { value: 'Data science', label: t`Data science` },
+    { value: 'Developer experience', label: t`Developer experience` },
+    { value: 'Cybersecurity', label: t`Cybersecurity (blue or purple team)` },
+    { value: 'DevOps and observability', label: t`DevOps and observability` },
 
     { value: 'Something else', label: 'Something else' },
   ];
 };
 
-export const getProfessionOptions = () => {};
+export const getProfessionOptions = () => [
+  {
+    id: 'work_segment',
+    value: 'work',
+    label: t`Work`,
+  },
+  {
+    id: 'education_segment',
+    value: 'education',
+    label: t`Education`,
+  },
+  {
+    id: 'personal_segment',
+    value: 'personal',
+    label: t`Personal`,
+  },
+];
