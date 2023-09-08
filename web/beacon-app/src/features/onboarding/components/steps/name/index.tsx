c736c022ac3db365c4833a8796d02e5cb40e814d
daniellemaxwell <danielle@rotational.io>
2023-09-08 11:58:21 -0400

    Removes repeated call

--- web/beacon-app/src/features/onboarding/components/steps/name/index.tsx
+++ web/beacon-app/src/features/onboarding/components/steps/name/index.tsx
@@ -21,7 +21,6 @@ const NameStep = () => {
       },
     };
     updateMember(payload);
-    increaseStep();
   };
 
   useEffect(() => {
--- web/beacon-app/src/features/onboarding/components/steps/organization/index.tsx
+++ web/beacon-app/src/features/onboarding/components/steps/organization/index.tsx
@@ -26,7 +26,6 @@ const OrganizationStep = () => {
       },
     };
     updateMember(payload);
-    increaseStep();
   };
 
   useEffect(() => {
