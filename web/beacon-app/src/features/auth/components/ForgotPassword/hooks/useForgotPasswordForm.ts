43c7ab6f0ecf955871e481d31d1858b66b6f6450
Danielle <danielle@rotational.io>
2023-09-18 14:28:07 -0400

    Adds forgot password view (#717)

---
+++ web/beacon-app/src/features/auth/components/ForgotPassword/hooks/useForgotPasswordForm.ts
@@ -0,0 +1,21 @@
+import { useFormik } from 'formik';
+import { object, string } from 'yup';
+
+export const FORM_INITIAL_VALUES = {
+  email: '',
+} as any;
+
+export const FORM_VALIDATION_SCHEMA = object({
+  email: string()
+    .trim()
+    .email('Please enter a valid email address.')
+    .required('Email is required.'),
+});
+
+export const FORM_OPTIONS = (onSubmit: any) => ({
+  initialValues: FORM_INITIAL_VALUES,
+  validationSchema: FORM_VALIDATION_SCHEMA,
+  onSubmit,
+});
+
+export const useForgotPasswordForm = (onSubmit: any) => useFormik(FORM_OPTIONS(onSubmit));
