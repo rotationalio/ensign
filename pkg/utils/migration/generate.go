4e637df3513c65858095d1b9ecb136f0fe80e324
Benjamin Bengfort <benjamin@bengfort.com>
2023-09-22 08:24:06 -0500

    clean up migration

--- pkg/utils/migration/generate.go
+++
@@ -1,3 +0,0 @@
-package migration
-
-//go:generate protoc -I=$GOPATH/src/github.com/rotationalio/ensign/proto --go_out=. --go_opt=module=github.com/rotationalio/ensign/pkg/utils/migration migration/migration.proto
