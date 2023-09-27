0e2de1e34d3dcbebaebc613edcacc708f9e2fa7a
Patrick Deziel <deziel.patrick@gmail.com>
2023-09-27 14:39:12 -0500

    Reset password token

--- pkg/quarterdeck/db/errors.go
+++ pkg/quarterdeck/db/errors.go
@@ -3,9 +3,12 @@ package db
 import "errors"
 
 var (
-	ErrTokenMissingEmail = errors.New("email verification token is missing email address")
-	ErrTokenExpired      = errors.New("email verification token has expired")
-	ErrInvalidSecret     = errors.New("invalid secret for email token verification")
-	ErrTokenInvalid      = errors.New("email verification token has invalid signature")
-	ErrSQLite3Conn       = errors.New("could not get sqlite3 connection for backups")
+	ErrMissingEmail       = errors.New("email address is required")
+	ErrMissingUserID      = errors.New("user id is required")
+	ErrTokenMissingEmail  = errors.New("email verification token is missing email address")
+	ErrTokenMissingUserID = errors.New("email verification token is missing user id")
+	ErrTokenExpired       = errors.New("email verification token has expired")
+	ErrInvalidSecret      = errors.New("invalid secret for email token verification")
+	ErrTokenInvalid       = errors.New("email verification token has invalid signature")
+	ErrSQLite3Conn        = errors.New("could not get sqlite3 connection for backups")
 )
--- pkg/quarterdeck/db/tokens.go
+++ pkg/quarterdeck/db/tokens.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/oklog/ulid/v2"
+	"github.com/rotationalio/ensign/pkg/utils/ulids"
 	"github.com/vmihailenco/msgpack/v5"
 )
 
@@ -21,17 +23,15 @@ const (
 // days.
 func NewVerificationToken(email string) (token *VerificationToken, err error) {
 	if email == "" {
-		return nil, errors.New("email address is required")
+		return nil, ErrMissingEmail
 	}
 
 	token = &VerificationToken{
-		Email:     email,
-		ExpiresAt: time.Now().AddDate(0, 0, 7),
-		Nonce:     make([]byte, nonceLength),
+		Email: email,
 	}
 
-	if _, err = rand.Read(token.Nonce); err != nil {
-		return nil, fmt.Errorf("could not generate token: %w", err)
+	if token.SigningInfo, err = NewSigningInfo(time.Hour * 24 * 7); err != nil {
+		return nil, err
 	}
 	return token, nil
 }
@@ -39,13 +39,8 @@ func NewVerificationToken(email string) (token *VerificationToken, err error) {
 // VerificationToken packages an email address with random data and an expiration time
 // so that it can be serialized and hashed into a token which can be sent to users.
 type VerificationToken struct {
-	Email     string    `msgpack:"email"`
-	ExpiresAt time.Time `msgpack:"expires_at"`
-	Nonce     []byte    `msgpack:"nonce"`
-}
-
-func (t *VerificationToken) IsExpired() bool {
-	return t.ExpiresAt.Before(time.Now())
+	Email string `msgpack:"email"`
+	SigningInfo
 }
 
 // Sign creates a base64 encoded string from the token data so that it can be sent to
@@ -57,23 +52,7 @@ func (t *VerificationToken) Sign() (_ string, secret []byte, err error) {
 		return "", nil, err
 	}
 
-	// Compute hash with a random 64 byte key
-	key := make([]byte, keyLength)
-	if _, err = rand.Read(key); err != nil {
-		return "", nil, err
-	}
-
-	mac := hmac.New(sha256.New, key)
-	if _, err = mac.Write(data); err != nil {
-		return "", nil, err
-	}
-
-	// Include the nonce with the key so that the token can be reconstructed later
-	secret = make([]byte, nonceLength+keyLength)
-	copy(secret[0:nonceLength], t.Nonce)
-	copy(secret[nonceLength:], key)
-
-	return base64.RawURLEncoding.EncodeToString(mac.Sum(nil)), secret, nil
+	return t.signData(data)
 }
 
 // Verify checks that a token was signed with the secret and is not expired.
@@ -97,6 +76,122 @@ func (t *VerificationToken) Verify(signature string, secret []byte) (err error) 
 		return err
 	}
 
+	return t.verifyData(data, signature, secret)
+}
+
+// NewResetToken creates a token struct from a user ID that expires in 15 minutes.
+func NewResetToken(id ulid.ULID) (token *ResetToken, err error) {
+	if ulids.IsZero(id) {
+		return nil, ErrMissingUserID
+	}
+
+	token = &ResetToken{
+		UserID: id,
+	}
+
+	if token.SigningInfo, err = NewSigningInfo(time.Minute * 15); err != nil {
+		return nil, err
+	}
+
+	if _, err = rand.Read(token.Nonce); err != nil {
+		return nil, fmt.Errorf("could not generate token: %w", err)
+	}
+	return token, nil
+}
+
+// ResetToken packages a user ID with random data and an expiration time so that it can
+// be serialized and hashed into a token which can be sent to users.
+type ResetToken struct {
+	UserID ulid.ULID `msgpack:"user_id"`
+	SigningInfo
+}
+
+// Sign creates a base64 encoded string from the token data so that it can be sent to
+// users as part of a URL. The returned secret should be stored in the database so that
+// the string can be recomputed when verifying a user provided token.
+func (t *ResetToken) Sign() (_ string, secret []byte, err error) {
+	var data []byte
+	if data, err = msgpack.Marshal(t); err != nil {
+		return "", nil, err
+	}
+
+	return t.signData(data)
+}
+
+// Verify checks that a token was signed with the secret and is not expired.
+func (t *ResetToken) Verify(signature string, secret []byte) (err error) {
+	if ulids.IsZero(t.UserID) {
+		return ErrTokenMissingUserID
+	}
+
+	if t.IsExpired() {
+		return ErrTokenExpired
+	}
+
+	if len(secret) != nonceLength+keyLength {
+		return ErrInvalidSecret
+	}
+
+	// Serialize the struct with the nonce from the secret
+	t.Nonce = secret[0:nonceLength]
+	var data []byte
+	if data, err = msgpack.Marshal(t); err != nil {
+		return err
+	}
+
+	return t.verifyData(data, signature, secret)
+}
+
+// Create new signing info with a time expiration.
+func NewSigningInfo(expires time.Duration) (info SigningInfo, err error) {
+	if expires == 0 {
+		return info, errors.New("expiration is required")
+	}
+
+	info = SigningInfo{
+		ExpiresAt: time.Now().Add(expires),
+		Nonce:     make([]byte, nonceLength),
+	}
+
+	if _, err = rand.Read(info.Nonce); err != nil {
+		return info, fmt.Errorf("could not generate signing info: %w", err)
+	}
+	return info, nil
+}
+
+// SigningInfo contains an expiration time and a nonce that is used to sign the token.
+type SigningInfo struct {
+	ExpiresAt time.Time `msgpack:"expires_at"`
+	Nonce     []byte    `msgpack:"nonce"`
+}
+
+func (d SigningInfo) IsExpired() bool {
+	return d.ExpiresAt.Before(time.Now())
+}
+
+// Create a signature from raw data and a nonce. The resulting signature is safe to be
+// used in a URL.
+func (d SigningInfo) signData(data []byte) (_ string, secret []byte, err error) {
+	// Compute hash with a random 64 byte key
+	key := make([]byte, keyLength)
+	if _, err = rand.Read(key); err != nil {
+		return "", nil, err
+	}
+
+	mac := hmac.New(sha256.New, key)
+	if _, err = mac.Write(data); err != nil {
+		return "", nil, err
+	}
+
+	// Include the nonce with the key so that the token can be reconstructed later
+	secret = make([]byte, nonceLength+keyLength)
+	copy(secret[0:nonceLength], d.Nonce)
+	copy(secret[nonceLength:], key)
+	return base64.RawURLEncoding.EncodeToString(mac.Sum(nil)), secret, nil
+}
+
+// Verify data using the signature and secret.
+func (d SigningInfo) verifyData(data []byte, signature string, secret []byte) (err error) {
 	// Compute hash to verify the user token
 	mac := hmac.New(sha256.New, secret[nonceLength:])
 	if _, err = mac.Write(data); err != nil {
