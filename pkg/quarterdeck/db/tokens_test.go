19136f646e40b71de8accab997b9dc7fa2787ccb
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2023-09-28 12:28:37 -0500

    Reset password token (#747)

--- pkg/quarterdeck/db/tokens_test.go
+++ pkg/quarterdeck/db/tokens_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/rotationalio/ensign/pkg/quarterdeck/db"
+	"github.com/rotationalio/ensign/pkg/utils/ulids"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,7 +33,9 @@ func TestVerificationToken(t *testing.T) {
 
 	// Verification should fail if the token is missing an email address
 	verify := &db.VerificationToken{
-		ExpiresAt: time.Now().AddDate(0, 0, 7),
+		SigningInfo: db.SigningInfo{
+			ExpiresAt: time.Now().AddDate(0, 0, 7),
+		},
 	}
 	require.ErrorIs(t, verify.Verify(signature, secret), db.ErrTokenMissingEmail, "expected error when token is missing email address")
 
@@ -62,4 +65,98 @@ func TestVerificationToken(t *testing.T) {
 
 	// Successful verification
 	require.NoError(t, verify.Verify(signature, secret), "expected successful verification")
+}
+
+func TestResetToken(t *testing.T) {
+	t.Run("Valid Reset Token", func(t *testing.T) {
+		// Test that the reset token is created correctly
+		id := ulids.New()
+		token, err := db.NewResetToken(id)
+		require.NoError(t, err, "could not create reset token")
+
+		// Test signing a token
+		signature, secret, err := token.Sign()
+		require.NoError(t, err, "failed to sign token")
+
+		// Signing again should produce a different signature
+		differentSig, differentSecret, err := token.Sign()
+		require.NoError(t, err, "failed to sign token")
+		require.NotEqual(t, signature, differentSig, "expected different signatures")
+		require.NotEqual(t, secret, differentSecret, "expected different secrets")
+
+		// Should be able to verify the token
+		require.NoError(t, token.Verify(signature, secret), "expected successful verification")
+	})
+
+	t.Run("Missing ID", func(t *testing.T) {
+		// Should fail to create a token without an ID
+		_, err := db.NewResetToken(ulids.Null)
+		require.ErrorIs(t, err, db.ErrMissingUserID, "expected error when token is missing ID")
+	})
+
+	t.Run("Token Missing User ID", func(t *testing.T) {
+		// Token with missing user ID should be an error
+		token := &db.ResetToken{}
+		require.ErrorIs(t, token.Verify("", nil), db.ErrTokenMissingUserID, "expected error when token is missing ID")
+	})
+
+	t.Run("Token Expired", func(t *testing.T) {
+		// Token that is expired should be an error
+		token := &db.ResetToken{
+			SigningInfo: db.SigningInfo{
+				ExpiresAt: time.Now().AddDate(0, 0, -1),
+			},
+			UserID: ulids.New(),
+		}
+		require.ErrorIs(t, token.Verify("", nil), db.ErrTokenExpired, "expected error when token is expired")
+	})
+
+	t.Run("Wrong User ID", func(t *testing.T) {
+		// Sign a valid token
+		token, err := db.NewResetToken(ulids.New())
+		require.NoError(t, err, "could not create reset token")
+		signature, secret, err := token.Sign()
+		require.NoError(t, err, "failed to sign token")
+
+		// Verification should fail if the user ID is different
+		token.UserID = ulids.New()
+		require.ErrorIs(t, token.Verify(signature, secret), db.ErrTokenInvalid, "expected error when user ID is different")
+	})
+
+	t.Run("Invalid Signature", func(t *testing.T) {
+		// Sign a valid token
+		token, err := db.NewResetToken(ulids.New())
+		require.NoError(t, err, "could not create reset token")
+		_, secret, err := token.Sign()
+		require.NoError(t, err, "failed to sign token")
+
+		// Verification should fail if the signature is not decodable
+		require.Error(t, token.Verify("^&**(", secret), "expected error when signature is not decodable")
+
+		// Verification should fail if the signature was created with a different secret
+		otherToken, err := db.NewResetToken(token.UserID)
+		require.NoError(t, err, "could not create reset token")
+		otherSig, _, err := otherToken.Sign()
+		require.NoError(t, err, "failed to sign token")
+		require.ErrorIs(t, token.Verify(otherSig, secret), db.ErrTokenInvalid, "expected error when signature was created with a different secret")
+	})
+
+	t.Run("Invalid Secret", func(t *testing.T) {
+		// Sign a valid token
+		token, err := db.NewResetToken(ulids.New())
+		require.NoError(t, err, "could not create reset token")
+		signature, _, err := token.Sign()
+		require.NoError(t, err, "failed to sign token")
+
+		// Should error if the secret has the wrong length
+		require.ErrorIs(t, token.Verify(signature, nil), db.ErrInvalidSecret, "expected error when secret is nil")
+		require.ErrorIs(t, token.Verify(signature, []byte("wronglength")), db.ErrInvalidSecret, "expected error when secret is the wrong length")
+
+		// Verification should fail if the wrong secret is used
+		otherToken, err := db.NewResetToken(token.UserID)
+		require.NoError(t, err, "could not create reset token")
+		_, otherSecret, err := otherToken.Sign()
+		require.NoError(t, err, "failed to sign token")
+		require.ErrorIs(t, token.Verify(signature, otherSecret), db.ErrTokenInvalid, "expected error when wrong secret is used")
+	})
 }
