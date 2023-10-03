71ea2ebf7b2f98fe3e6df3bb834ee0d531b8e140
Cletus Razakou <masskoder@gmail.com>
2023-10-03 10:40:53 -0400

    remove unecessary files

--- web/beacon-app/src/utils/misc.tsx
+++ web/beacon-app/src/utils/misc.tsx
@@ -33,13 +33,6 @@ export function updateQueryStringValueWithoutNavigation(
   const newUrl = [window.location.pathname, currentSearchParams.toString()]
     .filter(Boolean)
     .join('?');
-  // alright, let's talk about this...
-  // Normally with remix, you'd update the params via useSearchParams from react-router-dom
-  // and updating the search params will trigger the search to update for you.
-  // However, it also triggers a navigation to the new url, which will trigger
-  // the loader to run which we do not want because all our data is already
-  // on the client and we're just doing client-side filtering of data we
-  // already have. So we manually call `window.history.pushState` to avoid
-  // the router from triggering the loader.
+
   window.history.replaceState(null, '', newUrl);
 }
