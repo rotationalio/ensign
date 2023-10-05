1fad022704d62cefb9d233158276c1dbe82b2ef2
daniellemaxwell <danielle@rotational.io>
2023-10-05 12:36:54 -0400

    Removes hard coded value from onboarding e2e test

--- web/beacon-app/cypress/e2e/Onboarding/Onboarding.ts
+++ web/beacon-app/cypress/e2e/Onboarding/Onboarding.ts
@@ -321,7 +321,9 @@ When('I select a first developer option', function () {
 And('I click a second developer option', function () {
     cy.get('[id="developer_segment').click({multiple: true});
 
-    cy.findByText(this.user.onboarding.dev_segment.option_two).should('exist').click();
+    cy.findByText(this.user.onboarding.dev_segment.option_two)
+      .should('exist')
+      .click();
 });
 
 And('I click a third developer option', function () {
@@ -333,9 +335,11 @@ And('I click a third developer option', function () {
 });
 
 Then('I should see that I cannot select any more developer options', function () {
-    cy.get('[id="developer_segment').click({multiple: true})
-      .should('exist')
-      .and('have.attr', 'aria-disabled', 'true');
+    cy.get('[id="developer_segment').click({multiple: true});
+
+    cy.findByText(this.user.onboarding.dev_segment.option_four)
+    .should('exist')
+    .and('have.attr', 'aria-disabled', 'true');
 });
 
 When('I click next to submit the onboarding form', () => {
