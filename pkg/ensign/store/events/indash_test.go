e0c81167d6b6274ccc73940c13c01621e68f737f
Benjamin Bengfort <benjamin@bengfort.com>
2023-10-05 11:39:13 -0500

    improve test coverage

--- pkg/ensign/store/events/indash_test.go
+++ pkg/ensign/store/events/indash_test.go
@@ -1,6 +1,7 @@
 package events_test
 
 import (
+	"bytes"
 	"encoding/base64"
 
 	"github.com/oklog/ulid/v2"
@@ -49,10 +50,62 @@ func (s *eventsTestSuite) TestLoadIndash() {
 	require := s.Require()
 	require.False(s.store.ReadOnly())
 
+	_, err := s.LoadAllFixtures()
+	require.NoError(err, "could not load fixtures")
 	defer s.ResetDatabase()
+
+	topicID := ulid.MustParse("01GTSN1139JMK1PS5A524FXWAZ")
+	iter := s.store.LoadIndash(topicID)
+	defer iter.Release()
+
+	hashes := make(map[string]struct{})
+	for iter.Next() {
+		// Make sure each hash is unique
+		hash, err := iter.Hash()
+		require.NoError(err, "could not retrieve hash from object")
+		hashes[base64.RawStdEncoding.EncodeToString(hash)] = struct{}{}
+
+		// Ensure hash is a suffix of the key
+		key := iter.Key()
+		require.True(bytes.HasSuffix(key, hash))
+
+		// Ensure topic ID is a prefix of the key
+		require.True(bytes.HasPrefix(key, topicID[:]))
+
+		// Ensure the key is composed correctly
+		require.Len(key, len(topicID[:])+2+len(hash))
+	}
+
+	require.NoError(iter.Error(), "expected no error iterating")
+	require.Len(hashes, 22, "expected the same number of hashes as events in the fixture")
 }
 
 func (s *readonlyEventsTestSuite) TestLoadIndash() {
 	require := s.Require()
 	require.True(s.store.ReadOnly())
+
+	topicID := ulid.MustParse("01GTSN1139JMK1PS5A524FXWAZ")
+	iter := s.store.LoadIndash(topicID)
+	defer iter.Release()
+
+	hashes := make(map[string]struct{})
+	for iter.Next() {
+		// Make sure each hash is unique
+		hash, err := iter.Hash()
+		require.NoError(err, "could not retrieve hash from object")
+		hashes[base64.RawStdEncoding.EncodeToString(hash)] = struct{}{}
+
+		// Ensure hash is a suffix of the key
+		key := iter.Key()
+		require.True(bytes.HasSuffix(key, hash))
+
+		// Ensure topic ID is a prefix of the key
+		require.True(bytes.HasPrefix(key, topicID[:]))
+
+		// Ensure the key is composed correctly
+		require.Len(key, len(topicID[:])+2+len(hash))
+	}
+
+	require.NoError(iter.Error(), "expected no error iterating")
+	require.Len(hashes, 22, "expected the same number of hashes as events in the fixture")
 }
