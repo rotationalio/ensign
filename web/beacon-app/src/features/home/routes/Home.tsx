6ecfa29eecec6500139d015e2281c468e4698b7c
daniellemaxwell <danielle@rotational.io>
2023-10-11 10:35:48 -0400

    Removes quick view from home page

--- web/beacon-app/src/features/home/routes/Home.tsx
+++ web/beacon-app/src/features/home/routes/Home.tsx
@@ -4,17 +4,12 @@ import { Heading } from '@rotational/beacon-core';
 import AppLayout from '@/components/layout/AppLayout';
 
 import QuickStart from '../components/QuickStart';
-import QuickViewSummary from '../components/QuickViewSummary';
 import WelcomeAttention from '../components/WelcomeAttention';
 
 export default function Home() {
   return (
     <AppLayout>
       <WelcomeAttention />
-      <Heading as="h1" className="mb-4 text-lg font-semibold">
-        <Trans>Quick View</Trans>
-      </Heading>
-      <QuickViewSummary />
       <Heading as="h1" className="pt-10 text-lg font-semibold">
         <Trans>Get Started</Trans>
       </Heading>
