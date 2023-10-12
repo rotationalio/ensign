42cec1d00ce8ea712ee38692ecf50e08fe4985c4
Danielle <danielle@rotational.io>
2023-10-12 14:26:13 -0400

    Replaces schedule office hours button with icon (#779)

--- web/beacon-app/src/features/home/routes/Home.tsx
+++ web/beacon-app/src/features/home/routes/Home.tsx
@@ -1,20 +1,12 @@
-import { Trans } from '@lingui/macro';
-import { Heading } from '@rotational/beacon-core';
-
 import AppLayout from '@/components/layout/AppLayout';
 
 import QuickStart from '../components/QuickStart';
-import QuickViewSummary from '../components/QuickViewSummary';
 import WelcomeAttention from '../components/WelcomeAttention';
 
 export default function Home() {
   return (
     <AppLayout>
       <WelcomeAttention />
-      <Heading as="h1" className="mb-4 text-lg font-semibold">
-        <Trans>Quick View</Trans>
-      </Heading>
-      <QuickViewSummary />
       <QuickStart />
     </AppLayout>
   );
