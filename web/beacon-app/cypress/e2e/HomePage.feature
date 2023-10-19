77f18992679506d58818ae514306cbdd58c31e78
daniellemaxwell <danielle@rotational.io>
2023-10-19 13:08:20 -0500

    Removes tests for components removed from the home page

--- web/beacon-app/cypress/e2e/HomePage.feature
+++ web/beacon-app/cypress/e2e/HomePage.feature
@@ -9,9 +9,15 @@ Scenario: Navigating the Beacon main page
     And I should see an org name
     And I should see Projects in the sidebar
     And I should see Team in the sidebar
-    And I should see a link to Docs in the sidebar
-    And I should see a link to Support in the sidebar
     And I should see a link to Profile in the sidebar
+
+    And I should see a link to Ensign U in the sidebar
+    And I should see a link to Use Cases in the sidebar
+    And I should see a link to Docs in the sidebar
+    And I should see a link to Data Playground in the sidebar
+    And I should see a link to SDKs in the sidebar
+    And I should see a link to Support in the sidebar
+
     And I should see a link to the About page in the sidebar footer
     Then I should be able to visit the About page if I click the link
     And I should see a link to the Contact Us page in the sidebar footer
@@ -20,40 +26,31 @@ Scenario: Navigating the Beacon main page
     Then I should be able to visit the Server Status page if I click the link
     
     And I should see the Welcome component
-    When I click the Start button
-    Then I should see the create project modal
-    When I click the close button
-    Then I should not see the create project modal
-    When I click on the Start button again
-    And I fill in the project name
-    And I fill in the project description
-    And I click the Create Project button
-    Then I should be redirected to the projects page
-    And I should see the new project in the project list table
-    Then I should see the number of projects increase to 1 on the projects page
-    When I go back to the main page
-    And I should see the number of projects increase to 1 on the main page
+    And I should see the welcome to Ensign video
+    When I click on the welcome video
+    Then I should see a modal open with a playable version of the video
+    When I click the close button the modal
+    Then I should not see the modal with the video
 
     And I should see the Set Up A New Project component
-    And I should see the Access Resources component
-    And I should see the Access button
-    Then I should be able to visit the external documentation site
-    
-    When I click on the Create button
-    Then I should see the create project modal
-    When I fill in the project name
-    And I fill in the project description
-    And I click the Create Project button
-    Then I should be redirected to the projects page
-    And I should see the new project in the project list table
-    And I should see the number of projects increase to 2 on the projects page
-    When I go back to the main page
-    Then I should see the number of projects increase increase to 2 on the main page
-    And I should not see the Welcome component
+    And I should see the Create Project button
+    When I click the Create Project button
+    Then I should see the Create Project modal
+    When I click the close button in the Create Project modal
+    Then I should not see the Create Project modal
 
+    And I should see the Starter Videos component
+    And I should see thumbnails for the starter Videos
+    When I click on a thumbnail
+    Then I should see a modal open with a playable version of the video
+    When I click the close button the modal
+    Then I should not see the modal with the video
+
+    And I should see the Schedule Office Hours icon
+    Then I should see that I will be able to visit the Schedule Office Hours page if I click the icon
     When I see the settings button
     And I click the settings button
-    Then I should visit the settings page
-    When I go back to the main page
+    Then I should see the settings page
+    When I return to the main page
     And I click the logout button
-    Then I should log out of the Beacon home page
+    Then I should be logged out of the Beacon home page
--- web/beacon-app/cypress/e2e/HomePage/HomePage.ts
+++ web/beacon-app/cypress/e2e/HomePage/HomePage.ts
@@ -64,145 +64,20 @@ And('I should see a link to the Server Status page in the sidebar footer', () =>
     cy.get('ul>li>a').eq(2).should('have.attr', 'href').and('eq', 'https://status.rotational.dev/')
   });
 
-And('I should see quickview data', () => {
-  const data = [
-    'Projects',
-    'Topics',
-    'API Keys',
-    'Data Storage'
-  ]
-    cy.get('h5').each(($e, index) => {
-    cy.wrap($e).should('have.text', data[index])
-    cy.wrap($e).siblings().should('not.be.empty')
-  })
-});
-
 And('I should see the Welcome component', () => {
   cy.get('[data-cy="projWelcome"]').should('be.visible')
 })
-
-When('I click the Start button', () => {
-  cy.get('[data-cy="startSetupBttn"]').should('be.visible').click()
-})
-
-Then('I should see the create project modal', () => {
-  cy.get('[data-testid="newProjectModal"]').should('be.visible')
-});
-
-When('I click the close button', () => {
-cy.get('[data-testid="newProjectModal"]').within(() => {
-  cy.get('button>svg').click()
-})
-});
-
-Then('I should not see the create project modal', () => {
-  cy.get('[data-testid="newProjectModal"]').should('not.exist')
-});
-
-When('I click on the Start button again', () => {
-  cy.get('[data-cy="startSetupBttn"]').should('be.visible').click()
-});
-
-And('I fill in the project name', () => {
-  cy.get('[data-cy="project-name"]').should('exist').type('My first project')
-});
-
-And('I fill in the project description', () => {
-  cy.get('[data-cy="project-description"]').should('exist').type('A new project to test Ensign!')
-});
-
-And('I click the Create Project button', () => {
-  cy.get('[data-cy="NewProjectButton"]').should('exist').click()
-});
-
-Then('I should be redirected to the projects page', () => {
-cy.location('pathname').should('include', 'app/projects')
-});
-
-And('I should see the new project in the project list table', () => {
-cy.get('[data-cy="projectTable"]').should('exist').within(() => {
-  cy.get('tbody>tr>td').eq(0).should('have.text', 'My first project')
-      cy.get('tbody>tr>td').eq(1).should('have.text', 'A new project to test Ensign!')
-      cy.get('tbody>tr>td').eq(2).should('have.text', 'Incomplete')
-      cy.get('tbody>tr>td').eq(3).should('have.text', '05/12/2023')
-  })
-});
-
-Then('I should see the number of projects increase to 1 on the projects page', () => {
-cy.get('h5').eq(0).should('have.text', 'Projects').siblings('p').should('have.text', 1)
-});
-
-When('I go back to the main page', () => {
-cy.go('back')
-});
-
-And('I should see the number of projects increase to 1 on the main page', () => {
-cy.get('h5').eq(0).should('have.text', 'Projects').siblings('p').should('have.text', 1)
-});
 
 And('I should see the Set Up A New Project component', () => {
   cy.get('[data-testid="cardlistitem"]').eq(0).should('be.visible')
 });
 
-And('I should see the Access Resources component', () => {
-  cy.get('[data-testid="cardlistitem"]').eq(1).should('be.visible')
-});
-
-And('I should see the Access button', () => {
-  cy.get('[data-testid="viewDocs"]').should('have.text', 'Access')
-});
-
-Then('I should be able to visit the external documentation site', () => {
-    cy.get('[data-testid="viewDocsLink"]').should('have.attr', 'href').and('eq', 'https://ensign.rotational.dev/getting-started/')
-});
-
-  When('I click on the Create button', () => {
+  When('I click on the Create Project button', () => {
     cy.get('[data-testid="set-new-project"]').should('be.visible').click()
 });
 
 Then('I should see the create project modal', () => {
     cy.get('[data-testid="newProjectModal"]').should('be.visible')
-});
-
-When('I fill in the project name', () => {
-    cy.get('[data-cy="project-name"]').should('exist').type('My second project')
-});
-
-And('I fill in the project description', () => {
-    cy.get('[data-cy="project-description"]').should('exist').type('One more project!')
-});
-
-And('I click the Create Project button', () => {
-    cy.get('[data-cy="NewProjectButton"]').should('exist').click()
-});
-
-Then('I should be redirected to the projects page', () => {
-  cy.location('pathname').should('include', 'app/projects')
-});
-
-And('I should see the new project in the project list table', () => {
-  cy.get('[data-cy="projectTable"]').should('be.visible').within(() => {
-    cy.get('tbody>tr>td').eq(5).should('have.text', 'My second project')
-        cy.get('tbody>tr>td').eq(6).should('have.text', 'One more project!')
-        cy.get('tbody>tr>td').eq(7).should('have.text', 'Incomplete')
-        cy.get('tbody>tr>td').eq(8).should('have.text', '05/12/2023')   
-  })
-});
-
-And('I should see the number of projects increase to 2 on the projects page', () => {
-  cy.get('h5').eq(0).should('have.text', 'Projects').siblings('p').should('have.text', 2)
-});
-
-When('I go back to the main page', () => {
-  cy.go('back')
-});
-
-Then('I should see the number of projects increase increase to 2 on the main page', () => {
-  cy.get('h5').eq(0).should('have.text', 'Projects').siblings('p').should('have.text', 2)
-});
-
-And('I should not see the Welcome component', () => {
-  cy.get('[data-cy="projWelcome"]').should('not.exist')
 });
 
 When('I see the settings button', () => {
@@ -218,7 +93,7 @@ Then('I should visit the settings page', () => {
   cy.location('pathname').should('eq', '/app/profile')
 });
 
-And('I go back to the main page', () => {
+When('I return to the main page', () => {
   cy.go('back')
 });
 
@@ -227,6 +102,6 @@ And('I click the logout button', () => {
   cy.findByText('Logout').click()
 });
 
-Then('I should log out of the Beacon home page', () => {
+Then('I should be logged out of the Beacon home page', () => {
   cy.location('pathname').should('eq', '/')
 });
