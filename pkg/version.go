a8494019531cf0fc967dc9052f2b2ae7bf98277e
Patrick Deziel <deziel.patrick@gmail.com>
2023-12-01 11:05:19 -0600

    Version bump to v0.12.2

--- pkg/version.go
+++ pkg/version.go
@@ -6,9 +6,9 @@ import "fmt"
 const (
 	VersionMajor         = 0
 	VersionMinor         = 12
-	VersionPatch         = 1
+	VersionPatch         = 2
 	VersionReleaseLevel  = "beta"
-	VersionReleaseNumber = 16
+	VersionReleaseNumber = 17
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/rotationalio/ensign/pkg.GitVersion=$(git rev-parse --short HEAD)'"
