234724af6585e7758393b9f893ea8a622f066fc9
daniellemaxwell <danielle@rotational.io>
2024-01-15 16:11:59 -0500

    Add sandbox banner component

--- web/beacon-app/src/components/layout/AppLayout.tsx
+++ web/beacon-app/src/components/layout/AppLayout.tsx
@@ -1,13 +1,12 @@
-// import { Trans } from '@lingui/macro';
 import { Container } from '@rotational/beacon-core';
 import React, { ReactNode, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { /* EXTERNAL_LINKS, */ PATH_DASHBOARD } from '@/application';
+import { PATH_DASHBOARD } from '@/application';
 import { useFetchProfile } from '@/features/members/hooks/useFetchProfile';
 import { isOnboardedMember } from '@/features/members/utils';
 
-// import Alert from '../common/Alert/Alert';
+import SandboxBanner from './SanboxBanner/SandboxBanner';
 import Topbar from './Topbar';
 
 type PageProps = {
@@ -30,22 +29,8 @@ function AppLayout({ children, Breadcrumbs }: PageProps) {
   return (
     <>
       <Topbar Breadcrumbs={Breadcrumbs} isOnboarded={isOnboarded} profileData={loaderData} />
-      {/* TODO: Move to a SandboxAlert component and add a check for the user's account type
-      and only display if they are a sandbox user. */}
-      {/*  <Alert>
-        <div className="flex h-auto w-full flex-col items-center justify-center gap-x-4 bg-[#192E5B] py-6 text-center font-bold text-white lg:flex-row">
-          <p>
-            <Trans>
-              You are using the Ensign Sandbox. Ready to deploy your models to production?
-            </Trans>
-          </p>
-          <div className="mt-4 rounded-md border border-white bg-[#316D3C] px-4 py-1 lg:mt-0">
-            <a href={EXTERNAL_LINKS.ENSIGN_PRICING} target="_blank" rel="noreferrer">
-              <Trans>Upgrade</Trans>
-            </a>
-          </div>
-        </div>
-      </Alert> */}
+      {/* TODO: Display SandboxBanner only to user's with the sandbox account type. */}
+      <SandboxBanner />
       <Container max={696} centered className="my-10 mt-8 px-4 xl:px-28">
         {children}
       </Container>
