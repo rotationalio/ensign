f697da91f3ddecc4f14f4f966f2183ca1440e35d
Benjamin Bengfort <benjamin@bengfort.com>
2024-07-03 11:14:30 -0500

    Revert "version bump v0.12.6"
    This reverts commit 8ff4b86038739bbed5d201b704007d7e3b3d424c.

--- pkg/version.go
+++ pkg/version.go
@@ -6,9 +6,9 @@ import "fmt"
 const (
 	VersionMajor         = 0
 	VersionMinor         = 12
-	VersionPatch         = 7
+	VersionPatch         = 6
 	VersionReleaseLevel  = "beta"
-	VersionReleaseNumber = 22
+	VersionReleaseNumber = 21
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/rotationalio/ensign/pkg.GitVersion=$(git rev-parse --short HEAD)'"
